b142d68537f6097ce7175b32100cd2384e030ec4
Zoey Mertes <zmertes@purdue.edu>
2021-02-15 19:23:49 -0500

    Add emergency alerts tracking

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,9 @@ GITROOT="$(git rev-parse --show-toplevel)"
 # PUPD pages
 cd "$GITROOT/pages" || exit 2
 xargs -n 1 curl -O < "$GITROOT/urls.txt"
+# Purdue Emergency Alerts Page
+curl https://www.purdue.edu/emergency/ -o emergency.html
+curl https://www.purdue.edu/emergency/news/News.php -o emergency-news.html
 
 # Timely Warnings
 cd "$GITROOT/timely-warnings" || exit 2
