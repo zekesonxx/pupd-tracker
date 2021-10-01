44d1c58bf73fe0cf27ac8f25f6657235e324de6c
Zoey Mertes <zmertes@purdue.edu>
2021-09-30 20:30:30 -0400

    Finally update to new website

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,6 +8,10 @@ xargs -n 1 curl -O < "$GITROOT/urls.txt"
 curl https://www.purdue.edu/emergency/ -o emergency.html
 curl https://www.purdue.edu/emergency/news/News.php -o emergency-news.html
 
+# Daily Crime Logs
+cd "$GITROOT/daily-crime-log-archives" || exit 2
+wget --random-wait https://www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/index.php -r -D www.purdue.edu -I ehps/police/statistics-policies/daily-crime-log-archives || true
+
 # Timely Warnings
 cd "$GITROOT/timely-warnings" || exit 2
 wget --random-wait https://www.purdue.edu/timely-warnings/ -r -D www.purdue.edu -I timely-warnings || true
