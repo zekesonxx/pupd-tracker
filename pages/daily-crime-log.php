e678b316a59dd2c248c12f25b6cff8765dd139f9
Automated <actions@users.noreply.github.com>
2021-12-08 13:04:05 +0000

    Latest data: Wed Dec  8 13:04:05 UTC 2021

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -646,7 +646,42 @@
   
     	<section class="content__group">
   					<h3>Wednesday, December 8, 2021</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="157">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="207">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="157">Theft</td>
+<td style="font-weight: 400;" width="154">2021PU01442</td>
+<td style="font-weight: 400;" width="168">12/7/21 9:55 AM</td>
+<td style="font-weight: 400;" width="166">12/7/21 10:22 AM</td>
+<td style="font-weight: 400;" width="207">Earhart Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="157">Theft-Motor Vehicle Parts</td>
+<td style="font-weight: 400;" width="154">2021PU01443</td>
+<td style="font-weight: 400;" width="168">12/7/21 10:55 AM</td>
+<td style="font-weight: 400;" width="166">12/7/21 11:17 AM</td>
+<td style="font-weight: 400;" width="207">1200 Blk Tower Dr</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="157">Theft</td>
+<td style="font-weight: 400;" width="154">2021PU01444</td>
+<td style="font-weight: 400;" width="168">12/3/21 3:30 PM</td>
+<td style="font-weight: 400;" width="166">12/7/21 2:38 PM</td>
+<td style="font-weight: 400;" width="207">Dauch Alumni Center</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
