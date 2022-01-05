acbb487f145cb132832948893e6e51a38184fc2a
Automated <actions@users.noreply.github.com>
2022-01-05 13:38:11 +0000

    Latest data: Wed Jan  5 13:38:11 UTC 2022

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -646,7 +646,7 @@
   
     	<section class="content__group">
   					<h3>Wednesday,&#160;January 5, 2022</h3>
-<p>Will be posted soon.</p>
+<p>Nothing to report.</p>
 		</section>
 	
           <div class="clearfix"></div>
