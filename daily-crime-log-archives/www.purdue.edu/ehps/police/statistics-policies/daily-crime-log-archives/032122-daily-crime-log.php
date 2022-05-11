24c8d823c2e4542e20756256a0866cc44d4e86b5
Automated <actions@users.noreply.github.com>
2022-05-11 14:31:17 +0000

    Latest data: Wed May 11 14:31:17 UTC 2022

--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/032122-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/032122-daily-crime-log.php
@@ -735,7 +735,7 @@
 <td style="font-weight: 400;" width="168">3/18/22 11:25 AM</td>
 <td style="font-weight: 400;" width="166">3/22/22 2:16 PM</td>
 <td style="font-weight: 400;" width="207">Hillenbrand Hall</td>
-<td style="font-weight: 400;" width="181">Pending Prosecutor Review</td>
+<td style="font-weight: 400;" width="181">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="157">Theft-Bike</td>
--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -656,7 +656,34 @@
   
     	<section class="content__group">
   					<h3>Wednesday, May 11, 2022</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="156">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="207">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft</td>
+<td style="font-weight: 400;" width="154">2022PU00649</td>
+<td style="font-weight: 400;" width="168">5/7/22 10:30 AM</td>
+<td style="font-weight: 400;" width="166">5/10/22 10:53 AM</td>
+<td style="font-weight: 400;" width="207">Wetherill Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2022PU00652</td>
+<td style="font-weight: 400;" width="168">5/10/22 6:08 PM</td>
+<td style="font-weight: 400;" width="166">5/10/22 6:49 PM</td>
+<td style="font-weight: 400;" width="207">Hawkins Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -702,8 +729,8 @@
 <td style="font-weight: 400;" width="154">2022PU00648</td>
 <td style="font-weight: 400;" width="168">5/10/22 12:25 AM</td>
 <td style="font-weight: 400;" width="166">5/10/22 1:07 AM</td>
-<td style="font-weight: 400;" width="207">100 Blk of South River Rd</td>
-<td style="font-weight: 400;" width="181">Arrest Made</td>
+<td style="font-weight: 400;" width="207">Not Clery Reportable</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="156">Drug Law Violation</td>
