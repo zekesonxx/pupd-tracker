885321be6aa08759dc47d950785fcac00e487494
Automated <actions@users.noreply.github.com>
2022-09-14 15:18:55 +0000

    Latest data: Wed Sep 14 15:18:55 UTC 2022

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -665,7 +665,50 @@
   
     	<section class="content__group">
   					<h3>Wednesday, September 14, 2022</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="156">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="207">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft</td>
+<td style="font-weight: 400;" width="154">2022PU01148</td>
+<td style="font-weight: 400;" width="168">9/13/22 8:39 AM</td>
+<td style="font-weight: 400;" width="166">9/13/22 8:56 AM</td>
+<td style="font-weight: 400;" width="207">Rawls Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft</td>
+<td style="font-weight: 400;" width="154">2022PU01151</td>
+<td style="font-weight: 400;" width="168">9/10/22 3:00 PM</td>
+<td style="font-weight: 400;" width="166">9/13/22 1:14 PM</td>
+<td style="font-weight: 400;" width="207">Birck Boilermaker Golf Complex</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft</td>
+<td style="font-weight: 400;" width="154">2022PU01152</td>
+<td style="font-weight: 400;" width="168">9/12/22 7:00 PM</td>
+<td style="font-weight: 400;" width="166">9/13/22 3:36 PM</td>
+<td style="font-weight: 400;" width="207">CoRec</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2022PU01153</td>
+<td style="font-weight: 400;" width="168">9/13/22 4:47 PM</td>
+<td style="font-weight: 400;" width="166">9/13/22 5:00 PM</td>
+<td style="font-weight: 400;" width="207">Hawkins Graduate House</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
