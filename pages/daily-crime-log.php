5cca8ca4e9d530cce2e82ca1a017ed26de6d693b
Automated <actions@users.noreply.github.com>
2022-10-28 15:19:03 +0000

    Latest data: Fri Oct 28 15:19:03 UTC 2022

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -651,7 +651,66 @@
   
     	<section class="content__group">
   					<h3>Friday, October 28, 2022</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="156">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft</td>
+<td style="font-weight: 400;" width="154">2022PU01469</td>
+<td style="font-weight: 400;" width="168">10/20/22 6:15 PM</td>
+<td style="font-weight: 400;" width="166">10/27/22 3:45 PM</td>
+<td style="font-weight: 400;" width="212">Wiley Dining Court</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Operating While Never Receiving</td>
+<td style="font-weight: 400;" width="154">2022PU01470</td>
+<td style="font-weight: 400;" width="168">10/27/22 4:45 PM</td>
+<td style="font-weight: 400;" width="166">10/27/22 6:07 PM</td>
+<td style="font-weight: 400;" width="212">Grant St Garage</td>
+<td style="font-weight: 400;" width="181">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Drug Law Violation</td>
+<td style="font-weight: 400;" width="154">2022PU01473</td>
+<td style="font-weight: 400;" width="168">10/27/22 10:36 PM</td>
+<td style="font-weight: 400;" width="166">10/27/22 11:30 PM</td>
+<td style="font-weight: 400;" width="212">Provanance Apartments</td>
+<td style="font-weight: 400;" width="181">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Theft</td>
+<td style="font-weight: 400;" width="154">2022PU01474</td>
+<td style="font-weight: 400;" width="168">10/28/22 1:06 AM</td>
+<td style="font-weight: 400;" width="166">10/28/22 1:21 AM</td>
+<td style="font-weight: 400;" width="212">CoRec</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Drug Law Violation</td>
+<td style="font-weight: 400;" width="154">CAD# 2022-143151</td>
+<td style="font-weight: 400;" width="168">10/26/22 10:49 PM</td>
+<td style="font-weight: 400;" width="166">10/26/22 10:49 PM</td>
+<td style="font-weight: 400;" width="212">Aspire Apartments</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="156">Criminal Mischief</td>
+<td style="font-weight: 400;" width="154">CAD# 2022-143173</td>
+<td style="font-weight: 400;" width="168">10/27/22 12:10 AM</td>
+<td style="font-weight: 400;" width="166">10/27/22 12:10 AM</td>
+<td style="font-weight: 400;" width="212">Alpha Omicron Pi</td>
+<td style="font-weight: 400;" width="181">Under Investigation 2022WLO2602</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -724,7 +783,7 @@
 <td style="font-weight: 400;" width="168">10/21/22 4:57 PM</td>
 <td style="font-weight: 400;" width="166">10/25/22 9:23 AM</td>
 <td style="font-weight: 400;" width="212">Wiley Hall</td>
-<td style="font-weight: 400;" width="181">Under Investigation</td>
+<td style="font-weight: 400;" width="181">Suspended</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="156">Theft-Bike</td>
