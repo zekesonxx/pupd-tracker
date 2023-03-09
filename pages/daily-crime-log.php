40928f3f2169d4ad344e5d709af492c8e18dd61a
Automated <actions@users.noreply.github.com>
2023-03-09 13:41:28 +0000

    Latest data: Thu Mar  9 13:41:28 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -661,7 +661,34 @@
   
     	<section class="content__group">
   					<h3>Thursday, March 9, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Criminal Mischief</td>
+<td style="font-weight: 400;" width="154">2023PU00349</td>
+<td style="font-weight: 400;" width="168">3/8/23 9:52 AM</td>
+<td style="font-weight: 400;" width="166">3/8/23 10:12 AM</td>
+<td style="font-weight: 400;" width="212">West State St/Sheetz St</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Drug Law Violation</td>
+<td style="font-weight: 400;" width="154">2023PU00351</td>
+<td style="font-weight: 400;" width="168">3/8/23 9:11 PM</td>
+<td style="font-weight: 400;" width="166">3/8/23 9:55 PM</td>
+<td style="font-weight: 400;" width="212">Tarkington Hall</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
