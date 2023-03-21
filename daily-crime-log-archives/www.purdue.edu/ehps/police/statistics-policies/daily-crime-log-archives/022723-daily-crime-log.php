a758f973acf45258408dd04160d9242e6fbc302b
Automated <actions@users.noreply.github.com>
2023-03-21 12:53:50 +0000

    Latest data: Tue Mar 21 12:53:50 UTC 2023

--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/022723-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/022723-daily-crime-log.php
@@ -994,7 +994,7 @@
 <td style="font-weight: 400;" width="168">2/24/23 9:25 PM</td>
 <td style="font-weight: 400;" width="166">2/24/23 9:43 PM</td>
 <td style="font-weight: 400;" width="212">Pi Beta Phi</td>
-<td style="font-weight: 400;" width="181">Pending Prosecutor Review</td>
+<td style="font-weight: 400;" width="181">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="156">Drug Law Violation</td>
--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -675,7 +675,50 @@
   
     	<section class="content__group">
   					<h3>Tuesday,&#160;March 21, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2023PU00388</td>
+<td style="font-weight: 400;" width="168">3/10/23 8:00 AM</td>
+<td style="font-weight: 400;" width="166">3/20/23 12:15 PM</td>
+<td style="font-weight: 400;" width="212">Hilltop</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Battery-Simple</td>
+<td style="font-weight: 400;" width="154">2023PU00389</td>
+<td style="font-weight: 400;" width="168">2/17/23 4:00 AM</td>
+<td style="font-weight: 400;" width="166">3/20/23 1:58 PM</td>
+<td style="font-weight: 400;" width="212">Hawkins Graudate House</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Harassment</td>
+<td style="font-weight: 400;" width="154">2023PU00391</td>
+<td style="font-weight: 400;" width="168">3/20/23 6:00 PM</td>
+<td style="font-weight: 400;" width="166">3/20/23 6:44 PM</td>
+<td style="font-weight: 400;" width="212">Meredith South Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Stalking</td>
+<td style="font-weight: 400;" width="154">CSA</td>
+<td style="font-weight: 400;" width="168">3/8/23 3:00 PM</td>
+<td style="font-weight: 400;" width="166">3/20/23 9:00 AM</td>
+<td style="font-weight: 400;" width="212">On-Campus Housing</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
