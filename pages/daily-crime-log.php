bf399aff56553cb7fe850c49487d1207675197c9
Automated <actions@users.noreply.github.com>
2023-03-31 12:58:23 +0000

    Latest data: Fri Mar 31 12:58:23 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -659,7 +659,26 @@
   
     	<section class="content__group">
   					<h3>Friday, March 31, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: center; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft</td>
+<td style="font-weight: 400;" width="154">2023PU00438</td>
+<td style="font-weight: 400;" width="168">3/30/23 10:07 AM</td>
+<td style="font-weight: 400;" width="166">3/30/23 10:25 AM</td>
+<td style="font-weight: 400;" width="212">Rankin Track &amp; Field</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
