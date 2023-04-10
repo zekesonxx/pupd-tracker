0e2d66568bc9a6226cd4d972cce788b439fbd0e6
Automated <actions@users.noreply.github.com>
2023-04-10 12:55:40 +0000

    Latest data: Mon Apr 10 12:55:40 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -687,7 +687,74 @@
   
     	<section class="content__group">
   					<h3>Monday, April 10, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft</td>
+<td style="font-weight: 400;" width="154">2023PU00490</td>
+<td style="font-weight: 400;" width="168">4/5/23 4:30 PM</td>
+<td style="font-weight: 400;" width="166">4/7/23 11:14 AM</td>
+<td style="font-weight: 400;" width="212">Honors College</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft</td>
+<td style="font-weight: 400;" width="154">2023PU00494</td>
+<td style="font-weight: 400;" width="168">4/8/23 7:30 PM</td>
+<td style="font-weight: 400;" width="166">4/8/23 7:47 PM</td>
+<td style="font-weight: 400;" width="212">Winifred Parker Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Minor Consumption</td>
+<td style="font-weight: 400;" width="154">2023PU00496</td>
+<td style="font-weight: 400;" width="168">4/9/23 12:25 AM</td>
+<td style="font-weight: 400;" width="166">4/9/23 12:43 AM</td>
+<td style="font-weight: 400;" width="212">Aspire</td>
+<td style="font-weight: 400;" width="181">Pending Prosecutor Review</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Trespassing</td>
+<td style="font-weight: 400;" width="154">2023PU00497</td>
+<td style="font-weight: 400;" width="168">4/9/23 1:46 AM</td>
+<td style="font-weight: 400;" width="166">4/9/23 2:10 AM</td>
+<td style="font-weight: 400;" width="212">Ross-Ade Stadium</td>
+<td style="font-weight: 400;" width="181">Pending Prosecutor Review</td>
+</tr>
+<tr>
+<td style="font-weight: 400;">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2023PU00498</td>
+<td style="font-weight: 400;" width="168">4/8/23 5:30 PM</td>
+<td style="font-weight: 400;" width="166">4/9/23 5:36 PM</td>
+<td style="font-weight: 400;" width="212">Wiley Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2023PU00499</td>
+<td style="font-weight: 400;" width="168">4/9/23 8:12 PM</td>
+<td style="font-weight: 400;" width="166">4/9/23 8:28 PM</td>
+<td style="font-weight: 400;" width="212">Pao Hall Visual &amp; Performing Arts</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft Of Property</td>
+<td style="font-weight: 400;" width="154">CAD#2023-048094</td>
+<td style="font-weight: 400;" width="168">4/6/23 3:25 PM</td>
+<td style="font-weight: 400;" width="166">4/6/23 3:25 PM</td>
+<td style="font-weight: 400;" width="212">Wilmeth Active Learning Center</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
