c6cbbd1ab7fd9421e8d940179aa79285cbfdff97
Automated <actions@users.noreply.github.com>
2023-05-02 13:29:06 +0000

    Latest data: Tue May  2 13:29:06 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -680,7 +680,50 @@
   
     	<section class="content__group">
   					<h3>Tuesday,&#160;May 2, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Criminal Mischief</td>
+<td style="font-weight: 400;" width="154">2023PU00648</td>
+<td style="font-weight: 400;" width="168">5/1/23 1:09 PM</td>
+<td style="font-weight: 400;" width="166">5/1/23 1:21 PM</td>
+<td style="font-weight: 400;" width="212">Slayter Center of Performing Arts</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft</td>
+<td style="font-weight: 400;" width="154">2023PU00649</td>
+<td style="font-weight: 400;" width="168">5/1/23 2:00 PM</td>
+<td style="font-weight: 400;" width="166">5/1/23 4:53 PM</td>
+<td style="font-weight: 400;" width="212">Ford Dining Court</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Drug Law Violation</td>
+<td style="font-weight: 400;" width="154">CAD#2023-060525</td>
+<td style="font-weight: 400;" width="168">4/28/23 11:01 PM</td>
+<td style="font-weight: 400;" width="166">4/28/23 11:01 PM</td>
+<td style="font-weight: 400;" width="212">Cary Residence Hall</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft Of Bike</td>
+<td style="font-weight: 400;" width="154">CAD#2023-061245</td>
+<td style="font-weight: 400;" width="168">4/30/23 2:51 PM</td>
+<td style="font-weight: 400;" width="166">4/30/23 2:51 PM</td>
+<td style="font-weight: 400;" width="212">Shreve Residence Hall</td>
+<td style="font-weight: 400;" width="181">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
