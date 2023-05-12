594f4fc837d194d04b2c12e20ab907040200c725
Automated <actions@users.noreply.github.com>
2023-05-12 12:23:55 +0000

    Latest data: Fri May 12 12:23:55 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -659,7 +659,26 @@
   
     	<section class="content__group">
   					<h3>Friday, May 12, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Drug Law Violation</td>
+<td style="font-weight: 400;" width="154">2023PU00710</td>
+<td style="font-weight: 400;" width="168">5/11/23 12:39 AM</td>
+<td style="font-weight: 400;" width="166">5/11/23 1:02 AM</td>
+<td style="font-weight: 400;" width="212">West State St/N McCutcheon Dr</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
