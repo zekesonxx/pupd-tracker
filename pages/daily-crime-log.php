8cf97b7f06c3f7fd4a339908cc1c2f0ac0dec904
Automated <actions@users.noreply.github.com>
2023-05-30 14:27:21 +0000

    Latest data: Tue May 30 14:27:21 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -680,7 +680,50 @@
   
     	<section class="content__group">
   					<h3>Tuesday,&#160;May 30, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="212">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Operating While Intoxicated</td>
+<td style="font-weight: 400;" width="154">2023PU00757</td>
+<td style="font-weight: 400;" width="168">5/27/23 1:53 AM</td>
+<td style="font-weight: 400;" width="166">5/27/23 2:26 AM</td>
+<td style="font-weight: 400;" width="212">US HWY 231 S/W State St</td>
+<td style="font-weight: 400;" width="181">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Operating While Intoxicated</td>
+<td style="font-weight: 400;" width="154">2023PU00758</td>
+<td style="font-weight: 400;" width="168">5/27/23 2:40 AM</td>
+<td style="font-weight: 400;" width="166">5/27/23 3:19 AM</td>
+<td style="font-weight: 400;" width="212">Airport Rd/US HWY 231 S&#160;</td>
+<td style="font-weight: 400;" width="181">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Fraud</td>
+<td style="font-weight: 400;" width="154">2023PU00760</td>
+<td style="font-weight: 400;" width="168">5/28/23 8:50 PM</td>
+<td style="font-weight: 400;" width="166">5/28/23 9:36 PM</td>
+<td style="font-weight: 400;" width="212">Crosswalk Commons</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2023PU00761</td>
+<td style="font-weight: 400;" width="168">5/27/23 1:00 PM</td>
+<td style="font-weight: 400;" width="166">5/29/23 2:23 PM</td>
+<td style="font-weight: 400;" width="212">First Street Towers West</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
