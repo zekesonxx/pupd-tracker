7217ac644f028c04757ddff783a882282364b768
Automated <actions@users.noreply.github.com>
2023-07-25 12:59:27 +0000

    Latest data: Tue Jul 25 12:59:27 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -690,7 +690,7 @@
   
     	<section class="content__group">
   					<h3>Tuesday,&#160;July 25, 2023</h3>
-<p>Will be posted soon.</p>
+<p>Nothing to report.</p>
 		</section>
 	
           <div class="clearfix"></div>
