2911b9a7ee6a8a6d279e9bf9f65ee75f44ba338f
Automated <actions@users.noreply.github.com>
2023-08-10 14:30:09 +0000

    Latest data: Thu Aug 10 14:30:09 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -676,7 +676,26 @@
   
     	<section class="content__group">
   					<h3>Thursday, August 10, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="223">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="181">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2023PU00990</td>
+<td style="font-weight: 400;" width="168">8/9/23 9:16 PM</td>
+<td style="font-weight: 400;" width="166">8/9/23 9:32 PM</td>
+<td style="font-weight: 400;" width="223">Elliott Hall</td>
+<td style="font-weight: 400;" width="181">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
