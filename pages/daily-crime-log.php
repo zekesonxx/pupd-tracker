af2ad63b8e3ecc8a1420b8243d2874a004f4e0e6
Automated <actions@users.noreply.github.com>
2023-10-24 12:55:14 +0000

    Latest data: Tue Oct 24 12:55:14 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -680,7 +680,122 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Tuesday, October 24, 2023</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="223">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="199">Disposition&#160;</th>
+</tr>
+<tr>
+<td width="187">Fraud</td>
+<td width="154">2023PU01525</td>
+<td width="168">10/21/23 12:32 PM</td>
+<td width="166">10/21/23 1:16 PM</td>
+<td width="223">Hilltop</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Fraud</td>
+<td width="154">2023PU01531</td>
+<td width="168">10/17/23 12:00 PM</td>
+<td width="166">10/23/23 10:33 AM</td>
+<td width="223">Aspire</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td>Fraud</td>
+<td>2023PU01532</td>
+<td>10/17/23 12:00 PM</td>
+<td>10/23/23 10:36 AM</td>
+<td>Aspire</td>
+<td>Under Investigation</td>
+</tr>
+<tr>
+<td>Theft</td>
+<td>2023PU01533</td>
+<td>10/21/23 3:30 PM</td>
+<td>10/23/23 1:00 PM</td>
+<td>Brown Laboratory of Chemistry</td>
+<td>Under Investigation</td>
+</tr>
+<tr>
+<td>Theft</td>
+<td>2023PU01534</td>
+<td>10/20/23 11:00 AM</td>
+<td>10/23/23 1:02 PM</td>
+<td>Brown Laboratory of Chemistry</td>
+<td>Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Theft</td>
+<td width="154">2023PU01535</td>
+<td width="168">10/9/23 11:00 AM</td>
+<td width="166">10/23/23 1:08 PM</td>
+<td width="223">Brown Laboratory of Chemistry</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Theft</td>
+<td width="154">2023PU01536</td>
+<td width="168">10/23/23 10:31 PM</td>
+<td width="166">10/23/23 10:44 PM</td>
+<td width="223">CoRec</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Battery-Simple</td>
+<td width="154">2023PU01537</td>
+<td width="168">10/24/23 12:07 AM</td>
+<td width="166">10/24/23 12:29 AM</td>
+<td width="223">Cary Quad West</td>
+<td width="199">Pending Prosecutor Review</td>
+</tr>
+<tr>
+<td width="187">Drug Law Violation</td>
+<td width="154">2023PU01538</td>
+<td width="168">10/24/23 1:15 AM</td>
+<td width="166">10/24/23 1:35 AM</td>
+<td width="223">Mitch Daniels Blvd/N Martin Jischke Dr</td>
+<td width="199">Arrest Made</td>
+</tr>
+<tr>
+<td width="187">Indecent Exposure</td>
+<td width="154">CAD# 2023-154923</td>
+<td width="168">10/20/23 2:37 PM</td>
+<td width="166">10/20/23 2:37 PM</td>
+<td width="223">Tower Dr</td>
+<td width="199">Nothing Further</td>
+</tr>
+<tr>
+<td width="187">Hit &amp; Run</td>
+<td width="154">CAD# 2023-155103</td>
+<td width="168">10/20/23 9:59 PM</td>
+<td width="166">10/20/23 9:59 PM</td>
+<td width="223">McCutcheon Parking Garage</td>
+<td width="199">Nothing Further</td>
+</tr>
+<tr>
+<td width="187">Drug Law Violation</td>
+<td width="154">CAD# 2023-155165</td>
+<td width="168">10/21/23 1:29 AM</td>
+<td width="166">10/21/23 1:29 AM</td>
+<td width="223">Alpha Epsilon PI</td>
+<td width="199">Nothing Further</td>
+</tr>
+<tr>
+<td width="187">Rape</td>
+<td width="154">CSA</td>
+<td width="168">10/1/22 12:00 AM</td>
+<td width="166">10/23/23 8:30 AM</td>
+<td width="223">On-Campus Location</td>
+<td width="199">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
