8f5adeff58d077c2ce6d83906c5b2f8c67c69b6e
Automated <actions@users.noreply.github.com>
2023-12-13 17:28:27 +0000

    Latest data: Wed Dec 13 17:28:27 UTC 2023

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -669,7 +669,26 @@
   
     	<section class="content__group">
   					<h3>Wednesday, December 13, 2023</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="223">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="199">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="187">Drug Law Violation</td>
+<td style="font-weight: 400;" width="154">2023PU01825</td>
+<td style="font-weight: 400;" width="168">12/12/23 12:18 PM</td>
+<td style="font-weight: 400;" width="166">12/12/23 1:39 PM</td>
+<td style="font-weight: 400;" width="223">Purdue Memorial Union</td>
+<td style="font-weight: 400;" width="199">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
