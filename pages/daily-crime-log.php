16f22ac0ffb4b493586410ad0aed3855b13d2809
Automated <actions@users.noreply.github.com>
2024-01-05 17:29:38 +0000

    Latest data: Fri Jan  5 17:29:38 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -655,7 +655,42 @@
   
     	<section class="content__group">
   					<h3>Friday, January 5, 2024</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="223">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="199">Disposition&#160;</th>
+</tr>
+<tr>
+<td width="187">Criminal Mischief</td>
+<td width="154">2024PU00010</td>
+<td width="168">1/4/24 7:00 AM</td>
+<td width="166">1/4/24 10:59 AM</td>
+<td width="223">Purdue Memorial Union</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Criminal Mischief</td>
+<td width="154">2024PU00011</td>
+<td width="168">12/22/23 4:00 PM</td>
+<td width="166">1/4/24 1:38 PM</td>
+<td width="223">Physics Building</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Theft</td>
+<td width="154">CAD# 2024-001589</td>
+<td width="168">1/3/24 4:51 PM</td>
+<td width="166">1/3/24 4:51 PM</td>
+<td width="223">Chi Omega</td>
+<td width="199">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
