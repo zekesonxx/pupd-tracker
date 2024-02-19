552410681b2f6588912fc286231fefa1ee05f5dd
Automated <actions@users.noreply.github.com>
2024-02-19 15:45:12 +0000

    Latest data: Mon Feb 19 15:45:12 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -683,7 +683,74 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Monday, February 19, 2024</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="187">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="223">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="199">Disposition&#160;</th>
+</tr>
+<tr>
+<td width="187">Theft-Vehicle</td>
+<td width="154">2024PU00245</td>
+<td width="168">2/16/24 9:36 AM</td>
+<td width="166">2/16/24 9:49 AM</td>
+<td width="223">Ford Dining Court</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Fraud</td>
+<td width="154">2024PU00249</td>
+<td width="168">2/16/24 2:09 PM</td>
+<td width="166">2/16/24 2:32 PM</td>
+<td width="223">Tarkington Hall</td>
+<td width="199">Under Investigation</td>
+</tr>
+<tr>
+<td width="187">Minor Consumption</td>
+<td width="154">2024PU00253</td>
+<td width="168">2/17/24 12:46 AM</td>
+<td width="166">2/17/24 2:22 AM</td>
+<td width="223">McCutcheon Hall</td>
+<td width="199">Arrest Made</td>
+</tr>
+<tr>
+<td width="187">Public Intoxication</td>
+<td width="154">2024PU00254</td>
+<td width="168">2/17/24 3:24 AM</td>
+<td width="166">2/17/24 3:38 AM</td>
+<td width="223">Chaney Hale Hall</td>
+<td width="199">Pending Prosecutor Review</td>
+</tr>
+<tr>
+<td width="187">Drug Law Violation</td>
+<td width="154">2024PU00255</td>
+<td width="168">2/17/24 10:54 PM</td>
+<td width="166">2/17/24 11:21 PM</td>
+<td width="223">Meredith Hall</td>
+<td width="199">Arrest Made</td>
+</tr>
+<tr>
+<td width="187">Minor Consumption</td>
+<td width="154">2024PU00256</td>
+<td width="168">2/18/24 1:15 AM</td>
+<td width="166">2/18/24 1:27 AM</td>
+<td width="223">McCutcheon Hall</td>
+<td width="199">Pending Prosecutor Review</td>
+</tr>
+<tr>
+<td width="187">Residential Entry</td>
+<td width="154">2024PU00259</td>
+<td width="168">2/18/24 1:30 AM</td>
+<td width="166">2/18/24 3:03 PM</td>
+<td width="223">Wiley Hall</td>
+<td width="199">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
