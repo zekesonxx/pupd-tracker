c508f175a50ffb30a5464d1946def69421ce6ed1
Automated <actions@users.noreply.github.com>
2024-05-08 12:25:34 +0000

    Latest data: Wed May  8 12:25:34 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -669,7 +669,34 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, May 8, 2024</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="215">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="200">Disposition&#160;</th>
+</tr>
+<tr>
+<td width="171">Theft</td>
+<td width="154">2024PU00750</td>
+<td width="168">5/6/24 1:30 PM</td>
+<td width="166">5/7/24 2:31 PM</td>
+<td width="215">Owen Hall</td>
+<td width="200">Unfounded</td>
+</tr>
+<tr>
+<td width="171">Operating While Intoxicated</td>
+<td width="154">2024PU00752</td>
+<td width="168">5/8/24 2:37 AM</td>
+<td width="166">5/8/24 3:03 AM</td>
+<td width="215">McCormick Rd/3rd St</td>
+<td width="200">Arrest Made</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -692,7 +719,7 @@ Will be posted soon.
 <td width="168">5/6/24 11:31 PM</td>
 <td width="166">5/6/24 11:57 PM</td>
 <td width="215">Marsteller St/Mitch Daniels Blvd</td>
-<td width="200">Under Investigation</td>
+<td width="200">Arrest Made</td>
 </tr>
 <tr>
 <td width="171">Resisting Law Enforcement</td>
