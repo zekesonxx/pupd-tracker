ddd8c7ee2267c443464e647e4c24ce8f3f9eaa19
Automated <actions@users.noreply.github.com>
2024-06-12 16:46:07 +0000

    Latest data: Wed Jun 12 16:46:07 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -669,7 +669,34 @@
   
     	<section class="content__group">
   					<h3>Wednesday, June 12, 2024</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="215">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="200">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Criminal Mischief</td>
+<td style="font-weight: 400;" width="154">2024PU00848</td>
+<td style="font-weight: 400;" width="168">6/6/24 2:46 PM</td>
+<td style="font-weight: 400;" width="166">6/6/24 2:46 PM</td>
+<td style="font-weight: 400;" width="215">Beck Agricultural Center</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Arson</td>
+<td style="font-weight: 400;" width="154">2024PU00850</td>
+<td style="font-weight: 400;" width="168">6/11/24 10:19 PM</td>
+<td style="font-weight: 400;" width="166">6/11/24 11:15 PM</td>
+<td style="font-weight: 400;" width="215">Heavilon Hall</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
