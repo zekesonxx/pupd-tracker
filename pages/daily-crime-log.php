c9c7d18dec43e01832318f08bd41af19d32d12ed
Automated <actions@users.noreply.github.com>
2024-07-02 15:45:08 +0000

    Latest data: Tue Jul  2 15:45:08 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -676,7 +676,42 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Tuesday, July 2, 2024</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="215">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="200">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Driving While Suspended</td>
+<td style="font-weight: 400;" width="154">CAD# 2024-101523</td>
+<td style="font-weight: 400;" width="168">6/28/24 10:39 PM</td>
+<td style="font-weight: 400;" width="166">6/28/24 10:39 PM</td>
+<td style="font-weight: 400;" width="215">Northwestern Ave/W Stadium Ave</td>
+<td style="font-weight: 400;" width="200">Documented 2024WL01444</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">CAD# 2024-101562</td>
+<td style="font-weight: 400;" width="168">6/28/24 11:55 PM</td>
+<td style="font-weight: 400;" width="166">6/28/24 11:55 PM</td>
+<td style="font-weight: 400;" width="215">Muinzer Apts</td>
+<td style="font-weight: 400;" width="200">Documented 2024WL01445</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Reckless Driver</td>
+<td style="font-weight: 400;" width="154">CAD# 2024-102127</td>
+<td style="font-weight: 400;" width="168">6/30/24 3:49 AM</td>
+<td style="font-weight: 400;" width="166">6/30/24 3:49 AM</td>
+<td style="font-weight: 400;" width="215">100 Blk N Grant St</td>
+<td style="font-weight: 400;" width="200">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
