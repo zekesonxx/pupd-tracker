7182aeda84ac695ecbc77a132cbb5fae7d041e87
Automated <actions@users.noreply.github.com>
2024-07-08 14:15:00 +0000

    Latest data: Mon Jul  8 14:15:00 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -688,7 +688,42 @@
   
     	<section class="content__group">
   					<h3>Monday, July 8, 2024</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="215">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="200">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-EMPV</td>
+<td style="font-weight: 400;" width="154">2024PU00917</td>
+<td style="font-weight: 400;" width="168">7/5/24 3:37 PM</td>
+<td style="font-weight: 400;" width="166">7/5/24 3:57 PM</td>
+<td style="font-weight: 400;" width="215">Tarkington Hall</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="154">2024PU00918</td>
+<td style="font-weight: 400;" width="168">7/5/24 8:43 PM</td>
+<td style="font-weight: 400;" width="166">7/5/24 9:01 PM</td>
+<td style="font-weight: 400;" width="215">Frieda Parker Hall</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="154">2024PU00919</td>
+<td style="font-weight: 400;" width="168">7/5/24 7:00 PM</td>
+<td style="font-weight: 400;" width="166">7/6/24 12:43 PM</td>
+<td style="font-weight: 400;" width="215">Hawkins Hall</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
