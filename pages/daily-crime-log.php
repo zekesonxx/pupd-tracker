713ad41e1337f41325ede380bbe015498d3bdb0e
Automated <actions@users.noreply.github.com>
2024-07-12 13:45:08 +0000

    Latest data: Fri Jul 12 13:45:08 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -660,7 +660,26 @@
   
     	<section class="content__group">
   					<h3>Friday, July 12, 2024</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="215">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="200">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Operating While Intoxicated/ Drug Law Violation/ Hit &amp; Run</td>
+<td style="font-weight: 400;" width="154">2024PU00937</td>
+<td style="font-weight: 400;" width="168">7/12/24 4:25 AM</td>
+<td style="font-weight: 400;" width="166">7/12/24 4:52 AM</td>
+<td style="font-weight: 400;" width="215">N University St/ 1st St</td>
+<td style="font-weight: 400;" width="200">Arrest Made</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
