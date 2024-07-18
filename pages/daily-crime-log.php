60a42e5e22ac55c3e04e81808ad0968d294dc58e
Automated <actions@users.noreply.github.com>
2024-07-18 12:58:06 +0000

    Latest data: Thu Jul 18 12:58:06 UTC 2024

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -667,7 +667,42 @@
   
     	<section class="content__group">
   					<h3>Thursday, July 18, 2024</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="154">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="168">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="166">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="215">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="200">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Battery</td>
+<td style="font-weight: 400;" width="154">2024PU00949</td>
+<td style="font-weight: 400;" width="168">7/12/24 Unknown Time</td>
+<td style="font-weight: 400;" width="166">7/16/24 12:07 AM</td>
+<td style="font-weight: 400;" width="215">Meredith South</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Sexual Assault</td>
+<td style="font-weight: 400;" width="154">2024PU00950</td>
+<td style="font-weight: 400;" width="168">7/12/24 Unknown Time</td>
+<td style="font-weight: 400;" width="166">7/16/24 12:07 AM</td>
+<td style="font-weight: 400;" width="215">Meredith South</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="154">2024PU00951</td>
+<td style="font-weight: 400;" width="168">7/16/24 9:00 PM</td>
+<td style="font-weight: 400;" width="166">7/16/24 11:00 PM</td>
+<td style="font-weight: 400;" width="215">TREC</td>
+<td style="font-weight: 400;" width="200">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -732,7 +767,7 @@
 <td style="font-weight: 400;" width="168">7/16/24 12:07 AM</td>
 <td style="font-weight: 400;" width="166">7/16/24 12:07 AM</td>
 <td style="font-weight: 400;" width="215">Meredith South Hall</td>
-<td style="font-weight: 400;" width="200">Under Investigation</td>
+<td style="font-weight: 400;" width="200">Reported in 2024PU00949</td>
 </tr>
 </tbody>
 </table>
