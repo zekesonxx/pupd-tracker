ee3cbce66aa0561cddbd327c7176324676dca89d
Automated <actions@users.noreply.github.com>
2024-11-15 13:35:49 +0000

    Latest data: Fri Nov 15 13:35:49 UTC 2024

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -654,7 +654,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Thursday, November 14, 2024</h3>
+  					<h3>Updated Friday, November 15, 2024</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -986,11 +986,6 @@
 <td width="204">Mahony, John M.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">10/12/25</td>
-</tr>
-<tr>
-<td width="204">Manning, Charles W.</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">11/16/24</td>
 </tr>
 <tr>
 <td width="204">Martinez, Ricardo</td>
