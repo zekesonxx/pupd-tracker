ba6ba7366a6dc969cc15b0261be3b24b57003273
Automated <actions@users.noreply.github.com>
2025-03-03 16:41:28 +0000

    Latest data: Mon Mar  3 16:41:28 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -654,7 +654,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Thursday, February 27, 2025</h3>
+  					<h3>Updated Monday, March 3, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1180,7 +1180,7 @@
 <tr>
 <td width="204">Smith, Jason M.</td>
 <td width="211">Entire WL Purdue Campus</td>
-<td width="76">1/5/26</td>
+<td width="76">3/3/30</td>
 </tr>
 <tr>
 <td width="204">Spar, Christopher A.</td>
@@ -1791,6 +1791,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
+        <p><em>Last modified: <span>March 3, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
