c53a58515e06364cbb1701f0eef4ef198ffd1aee
Automated <actions@users.noreply.github.com>
2025-03-05 12:33:43 +0000

    Latest data: Wed Mar  5 12:33:43 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -654,7 +654,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Monday, March 3, 2025</h3>
+  					<h3>Updated Wednesday, March 5, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -851,11 +851,6 @@
 <td width="204">Gallacher, Allen J.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">4/28/25</td>
-</tr>
-<tr>
-<td width="204">Galloway, James R.</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">3/5/25</td>
 </tr>
 <tr>
 <td width="204">Garcia-Gaeta, Juan A.</td>
@@ -1166,11 +1161,6 @@
 <td width="204">Skiles, Mark A.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">11/10/27</td>
-</tr>
-<tr>
-<td width="204">Smith, Clayton</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">3/5/25</td>
 </tr>
 <tr>
 <td width="204">Smith, Jason M.</td>
@@ -1789,9 +1779,10 @@
             
     
 
+
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>March 3, 2025</span></em></p>
+        <p><em>Last modified: <span>March 4, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
@@ -1804,7 +1795,7 @@
         
                 
                      <p>Contact <a href="../contact/index.php" rel="noopener" target="_blank">Purdue University Police Department</a> for accessibility issues with this page | <a href="https://www.purdue.edu/disabilityresources/">Accessibility Resources</a> | <a href="../contact/index.php">Contact Us</a> | <a href="https://collegescorecard.ed.gov/school/fields/?243780-Purdue-University-Main-Campus" target="_blank">DOE Degree Scorecards</a></p>
-    </article>
+            </article>
 </section>
 
 
