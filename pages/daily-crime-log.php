0fed6403b815b2ee07e98beedc7a70f6449bb679
Automated <actions@users.noreply.github.com>
2025-03-20 15:37:10 +0000

    Latest data: Thu Mar 20 15:37:10 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -672,7 +672,7 @@
   
     	<section class="content__group">
   					<h3>Thursday, March 20, 2025</h3>
-Will be posted soon.
+Nothing to report.
 		</section>
 	
           <div class="clearfix"></div>
@@ -843,7 +843,7 @@ Will be posted soon.
 <td style="font-weight: 400;" width="149">3/14/25 10:00 AM</td>
 <td style="font-weight: 400;" width="149">3/14/25 5:00 PM</td>
 <td style="font-weight: 400;" width="193">700 Mitch Daniels Blvd</td>
-<td style="font-weight: 400;" width="186">Under Investigation</td>
+<td style="font-weight: 400;" width="186">Unfounded</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="213">Theft-Bike</td>
@@ -1375,7 +1375,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>March 19, 2025</span></em></p>
+        <p><em>Last modified: <span>March 20, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
