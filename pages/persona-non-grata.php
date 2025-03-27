ded767ea695e02c19b63827c7be3e5306049bf03
Automated <actions@users.noreply.github.com>
2025-03-27 19:26:22 +0000

    Latest data: Thu Mar 27 19:26:22 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Tuesday, March 25, 2025</h3>
+  					<h3>Updated Thursday, March 27, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -811,11 +811,6 @@
 <td width="204">De Mello, Daniel</td>
 <td width="211">DSAI</td>
 <td width="76">9/9/25</td>
-</tr>
-<tr>
-<td width="204">Denney, Charles K.</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">3/15/26</td>
 </tr>
 <tr>
 <td width="204">Espey, Jacob D.</td>
@@ -1782,7 +1777,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>March 24, 2025</span></em></p>
+        <p><em>Last modified: <span>March 27, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
