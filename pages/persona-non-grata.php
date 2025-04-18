710a3c10421d5d9d8cad5bb94a6ffbafbb4793f9
Automated <actions@users.noreply.github.com>
2025-04-18 23:31:04 +0000

    Latest data: Fri Apr 18 23:31:04 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -758,6 +758,11 @@
 <td width="76">7/30/25</td>
 </tr>
 <tr>
+<td width="204">Choudhury, Soumitra</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">4/17/30</td>
+</tr>
+<tr>
 <td width="204">Christopher, David M.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">2/18/26</td>
@@ -1772,7 +1777,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>April 17, 2025</span></em></p>
+        <p><em>Last modified: <span>April 18, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
