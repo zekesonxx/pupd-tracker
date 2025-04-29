d880d38411d8e22d60a0fac26a65cf0824116090
Automated <actions@users.noreply.github.com>
2025-04-29 16:51:21 +0000

    Latest data: Tue Apr 29 16:51:21 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -686,7 +686,82 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Tuesday, April 29, 2025</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="213">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="150">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="149">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="149">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="193">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="186">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Drug Law Violation</td>
+<td style="font-weight: 400;" width="150">2025PU00569</td>
+<td style="font-weight: 400;" width="149">4/21/25 8:30 PM</td>
+<td style="font-weight: 400;" width="149">4/21/25 8:30 PM</td>
+<td style="font-weight: 400;" width="193">Purdue Agronomy Farm</td>
+<td style="font-weight: 400;" width="186">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Theft</td>
+<td style="font-weight: 400;" width="150">2025PU00625</td>
+<td style="font-weight: 400;" width="149">4/25/25 5:17 PM</td>
+<td style="font-weight: 400;" width="149">4/28/25 11:29 AM</td>
+<td style="font-weight: 400;" width="193">Stanley Coulter Hall</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Fraud</td>
+<td style="font-weight: 400;" width="150">2025PU00627</td>
+<td style="font-weight: 400;" width="149">4/28/25 12:43 PM</td>
+<td style="font-weight: 400;" width="149">4/28/25 12:58 PM</td>
+<td style="font-weight: 400;" width="193">Armstrong Hall</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Theft</td>
+<td style="font-weight: 400;" width="150">2025PU00628</td>
+<td style="font-weight: 400;" width="149">4/25/25 3:03 PM</td>
+<td style="font-weight: 400;" width="149">4/28/25 2:40 PM</td>
+<td style="font-weight: 400;" width="193">318 N Russell St</td>
+<td style="font-weight: 400;" width="186">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Hit &amp; Run</td>
+<td style="font-weight: 400;" width="150">2025PU00630</td>
+<td style="font-weight: 400;" width="149">4/28/25 3:45 PM</td>
+<td style="font-weight: 400;" width="149">4/28/25 9:30 PM</td>
+<td style="font-weight: 400;" width="193">Wiley Dining Court</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Minor Consumption</td>
+<td style="font-weight: 400;" width="150">2025PU00631</td>
+<td style="font-weight: 400;" width="149">4/28/25 11:02 PM</td>
+<td style="font-weight: 400;" width="149">4/28/25 11:13 PM</td>
+<td style="font-weight: 400;" width="193">Shreve Hall</td>
+<td style="font-weight: 400;" width="186">Pending Prosecutor Review</td>
+</tr>
+<tr>
+<td style="font-weight: 400;">Reckless Driver</td>
+<td style="font-weight: 400;" width="150">CAD# 2025-063474</td>
+<td style="font-weight: 400;" width="149">4/25/25 10:38 PM</td>
+<td style="font-weight: 400;">4/25/25 10:38 PM</td>
+<td style="font-weight: 400;">Mitch Daniels Blvd/S Grant St</td>
+<td style="font-weight: 400;">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Reckless Driver</td>
+<td style="font-weight: 400;" width="150">CAD# 2025-063866</td>
+<td style="font-weight: 400;" width="149">4/26/25 4:54 PM</td>
+<td style="font-weight: 400;" width="149">4/26/25 4:54 PM</td>
+<td style="font-weight: 400;" width="193">US HWY 231 S/S River Rd</td>
+<td style="font-weight: 400;" width="186">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1361,7 +1436,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>April 28, 2025</span></em></p>
+        <p><em>Last modified: <span>April 29, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
