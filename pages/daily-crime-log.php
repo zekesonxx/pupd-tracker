f36c6eed3b228ecd6cf06bcfa267215726012fcb
Automated <actions@users.noreply.github.com>
2025-05-14 14:18:24 +0000

    Latest data: Wed May 14 14:18:24 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -679,7 +679,58 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, May 14, 2025</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="213">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="173">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="149">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="149">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="193">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="186">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Drug Law Violation</td>
+<td style="font-weight: 400;" width="173">2025PU00719</td>
+<td style="font-weight: 400;" width="149">5/13/25 10:08 AM</td>
+<td style="font-weight: 400;" width="149">5/13/25 10:41 AM</td>
+<td style="font-weight: 400;" width="193">1300 Blk W Stadium Ave</td>
+<td style="font-weight: 400;" width="186">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Theft-Bike</td>
+<td style="font-weight: 400;" width="173">2025PU00720</td>
+<td style="font-weight: 400;" width="149">3/11/25 1:30 PM</td>
+<td style="font-weight: 400;" width="149">5/13/25 12:56 PM</td>
+<td style="font-weight: 400;" width="193">Wilmeth Active Learning Center</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Theft-EMPV</td>
+<td style="font-weight: 400;" width="173">2025PU00722</td>
+<td style="font-weight: 400;" width="149">5/9/25 11:00 AM</td>
+<td style="font-weight: 400;" width="149">5/13/25 2:34 PM</td>
+<td style="font-weight: 400;" width="193">Owen Hall</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Theft-Bike</td>
+<td style="font-weight: 400;" width="173">2025PU00723</td>
+<td style="font-weight: 400;" width="149">5/5/25 10:00 AM</td>
+<td style="font-weight: 400;" width="149">5/13/25 3:10 PM</td>
+<td style="font-weight: 400;" width="193">Wiley Hall</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="213">Criminal Mischief</td>
+<td style="font-weight: 400;" width="173">2025PU00725</td>
+<td style="font-weight: 400;" width="149">5/13/25 6:28 PM</td>
+<td style="font-weight: 400;" width="149">5/13/25 6:45 PM</td>
+<td style="font-weight: 400;" width="193">Winifred Parker Hall</td>
+<td style="font-weight: 400;" width="186">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1316,7 +1367,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>May 13, 2025</span></em></p>
+        <p><em>Last modified: <span>May 14, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
