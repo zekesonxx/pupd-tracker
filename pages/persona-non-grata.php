1616fd7f653c01f269e9ad24ca75993c85d9675d
Automated <actions@users.noreply.github.com>
2025-05-21 19:14:23 +0000

    Latest data: Wed May 21 19:14:23 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Tuesday, May 20, 2025</h3>
+  					<h3>Updated Wednesday, May 21, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1278,6 +1278,11 @@
 <td width="76">6/13/25</td>
 </tr>
 <tr>
+<td width="204">Zhou, Bei</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">5/21/30</td>
+</tr>
+<tr>
 <td width="204">Ziegler, Jonathan J.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">3/15/27</td>
@@ -1772,7 +1777,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>May 20, 2025</span></em></p>
+        <p><em>Last modified: <span>May 21, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
