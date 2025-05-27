dd93f48627205fc19d227388e938185f2be069ce
Automated <actions@users.noreply.github.com>
2025-05-27 15:39:06 +0000

    Latest data: Tue May 27 15:39:06 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Friday, May 23, 2025</h3>
+  					<h3>Updated Tuesday, May 27, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -873,6 +873,11 @@
 <td width="76">3/8/29</td>
 </tr>
 <tr>
+<td width="204">Garcia, Roberto H.</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">5/27/26</td>
+</tr>
+<tr>
 <td width="204">Garcia-Gaeta, Juan A.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">3/19/30</td>
@@ -949,7 +954,7 @@
 </tr>
 <tr>
 <td width="204">James, Ethen O.</td>
-<td width="211">Entire WL Purdue Campus</td>
+<td width="211">All Parking Garages</td>
 <td width="76">5/22/25</td>
 </tr>
 <tr>
@@ -1787,7 +1792,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>May 23, 2025</span></em></p>
+        <p><em>Last modified: <span>May 27, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
