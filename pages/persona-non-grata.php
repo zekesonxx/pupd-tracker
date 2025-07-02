200ae57ae649b354967e08a6ef62f25ab0f5aaf0
Automated <actions@users.noreply.github.com>
2025-07-02 13:20:05 +0000

    Latest data: Wed Jul  2 13:20:05 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -605,11 +605,6 @@
         
                 
     
-    <li role="none"><a href="indy-daily-crime-log.php" role="menuitem">Indianapolis Daily Crime Log</a></li>
-                                                                            
-        
-                
-    
     <li role="none"><a href="drugs-alcohol.php" role="menuitem">Drugs and Alcohol Policy</a></li>
                                                                             
         
@@ -659,7 +654,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Friday, June 27, 2025</h3>
+  					<h3>Updated Wednesday, July 2, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -950,7 +945,7 @@
 <tr>
 <td width="204">James, Ethen O.</td>
 <td width="211">All Parking Garages</td>
-<td width="76">5/22/25</td>
+<td width="76">5/22/26</td>
 </tr>
 <tr>
 <td width="204">Jung, Isaac J.</td>
@@ -960,7 +955,7 @@
 <tr>
 <td width="204">Khan, Hani H.</td>
 <td width="211">All Residence Halls</td>
-<td width="76">5/29/25</td>
+<td width="76">5/29/26</td>
 </tr>
 <tr>
 <td width="204">Khichi, Ali A.</td>
@@ -1777,7 +1772,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>June 27, 2025</span></em></p>
+        <p><em>Last modified: <span>July 2, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
