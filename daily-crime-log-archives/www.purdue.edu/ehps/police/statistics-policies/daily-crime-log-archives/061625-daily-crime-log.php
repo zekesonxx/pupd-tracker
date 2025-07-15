8138ebd750d3a186c3f0411359b142817ee30ba2
Automated <actions@users.noreply.github.com>
2025-07-15 13:35:26 +0000

    Latest data: Tue Jul 15 13:35:26 UTC 2025

--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/061625-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/061625-daily-crime-log.php
@@ -782,7 +782,7 @@
 <th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
 </tr>
 <tr>
-<td style="font-weight: 400;" width="171">Trespass/ Theft</td>
+<td style="font-weight: 400;" width="171">Trespass</td>
 <td style="font-weight: 400;" width="172">2025PU00809</td>
 <td style="font-weight: 400;" width="148">6/13/25 12:20 AM</td>
 <td style="font-weight: 400;" width="148">6/13/25 11:56 AM</td>
@@ -1303,7 +1303,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 7, 2025</span></em></p>
+        <p><em>Last modified: <span>July 15, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/070725-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/070725-daily-crime-log.php
@@ -646,12 +646,12 @@
 <th scope="col" style="text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
 </tr>
 <tr>
-<td width="171">Trespass</td>
+<td width="171">Trespass/ Theft</td>
 <td width="172">2025PU00869</td>
 <td width="148">6/13/25 12:40 AM</td>
 <td width="148">6/13/25 12:40 AM</td>
 <td width="201">Ross-Ade Stadium</td>
-<td width="189">Under Investigation</td>
+<td width="189">Suspended</td>
 </tr>
 <tr>
 <td width="171">Theft</td>
@@ -1335,7 +1335,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 11, 2025</span></em></p>
+        <p><em>Last modified: <span>July 15, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
