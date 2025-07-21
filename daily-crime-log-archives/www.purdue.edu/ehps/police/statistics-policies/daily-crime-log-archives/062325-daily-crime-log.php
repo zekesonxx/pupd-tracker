d63ba1e753bc1b350fa353d44762ef1999fbd373
Automated <actions@users.noreply.github.com>
2025-07-21 14:54:14 +0000

    Latest data: Mon Jul 21 14:54:14 UTC 2025

--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/062325-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/062325-daily-crime-log.php
@@ -651,7 +651,7 @@
 <td style="font-weight: 400;" width="148">6/24/25 11:36 AM</td>
 <td style="font-weight: 400;" width="148">6/25/25 1:54 PM</td>
 <td style="font-weight: 400;" width="201">Wiley Hall</td>
-<td style="font-weight: 400;" width="189">Pending Prosecutor Review</td>
+<td style="font-weight: 400;" width="189">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Child Molestation</td>
@@ -1380,7 +1380,7 @@ Nothing to report.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 9, 2025</span></em></p>
+        <p><em>Last modified: <span>July 21, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
