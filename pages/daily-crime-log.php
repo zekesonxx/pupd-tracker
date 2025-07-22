81cf1c447ee6430e56294fc63805055fa6c421f5
Automated <actions@users.noreply.github.com>
2025-07-22 13:37:14 +0000

    Latest data: Tue Jul 22 13:37:14 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -686,7 +686,42 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Tuesday, July 22, 2025</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Animal Cruelty</td>
+<td style="font-weight: 400;" width="172">2025PU00899</td>
+<td style="font-weight: 400;" width="148">7/21/25 10:20 PM</td>
+<td style="font-weight: 400;" width="148">7/21/25 11:34 PM</td>
+<td style="font-weight: 400;" width="201">Brunner Animal Hospital</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Public Nudity</td>
+<td style="font-weight: 400;" width="172">CAD# 2025-109236</td>
+<td style="font-weight: 400;" width="148">7/19/25 1:36 AM</td>
+<td style="font-weight: 400;" width="148">7/19/25 1:36 AM</td>
+<td style="font-weight: 400;" width="201">Engineering Fountain</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Battery-Simple/ Operating While Intoxicated/ Disorderly Conduct/ Resisting Law Enforcement</td>
+<td style="font-weight: 400;" width="172">CAD# 2025-109800</td>
+<td style="font-weight: 400;" width="148">7/20/25 2:55 AM</td>
+<td style="font-weight: 400;" width="148">7/20/25 2:55 AM</td>
+<td style="font-weight: 400;" width="201">S River Rd/Ahlers Dr</td>
+<td style="font-weight: 400;" width="189">Documented 2025WL01687</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1209,7 +1244,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 21, 2025</span></em></p>
+        <p><em>Last modified: <span>July 22, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
