6f8cc1068ab2d89c6678a679fcb53302be23a140
Automated <actions@users.noreply.github.com>
2025-08-07 22:20:01 +0000

    Latest data: Thu Aug  7 22:20:01 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -893,6 +893,11 @@
 <td width="76">3/5/26</td>
 </tr>
 <tr>
+<td width="204">Goranson, David N.</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">8/7/26</td>
+</tr>
+<tr>
 <td width="204">Gore, Matthew D.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">3/7/26</td>
@@ -1767,7 +1772,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>August 6, 2025</span></em></p>
+        <p><em>Last modified: <span>August 7, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
