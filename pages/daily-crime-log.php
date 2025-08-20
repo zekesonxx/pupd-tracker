ddd3fb81404e7bde88ddfd264aad0b1cc16ef9e1
Automated <actions@users.noreply.github.com>
2025-08-20 14:19:06 +0000

    Latest data: Wed Aug 20 14:19:06 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -679,7 +679,42 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, August 20, 2025</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<td style="font-weight: 400;" width="171">Nature</td>
+<td style="font-weight: 400;" width="172">Case Number</td>
+<td style="font-weight: 400;" width="148">Date/Time Occurred</td>
+<td style="font-weight: 400;" width="148">Date/Time Reported</td>
+<td style="font-weight: 400;" width="201">General Location</td>
+<td style="font-weight: 400;" width="189">Disposition&#160;</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Hit &amp; Run</td>
+<td style="font-weight: 400;" width="172">2025PU00971</td>
+<td style="font-weight: 400;" width="148">8/19/25 3:00 PM</td>
+<td style="font-weight: 400;" width="148">8/19/25 3:59 PM</td>
+<td style="font-weight: 400;" width="201">696 S Russell St</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Driving Never Receiving License</td>
+<td style="font-weight: 400;" width="172">2025PU00972</td>
+<td style="font-weight: 400;" width="148">8/19/25 3:36 PM</td>
+<td style="font-weight: 400;" width="148">8/19/25 4:33 PM</td>
+<td style="font-weight: 400;" width="201">McCutcheon Dr/ Mitch Daniels Blvd</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Minor Consumption</td>
+<td style="font-weight: 400;" width="172">CAD# 2025-125243</td>
+<td style="font-weight: 400;" width="148">8/19/25 12:25 AM</td>
+<td style="font-weight: 400;" width="148">8/19/25 12:25 AM</td>
+<td style="font-weight: 400;" width="201">Mitch Daniels Blvd/N Martin Jischke Dr</td>
+<td style="font-weight: 400;" width="189">Documented 2025WL01942</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1292,7 +1327,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>August 19, 2025</span></em></p>
+        <p><em>Last modified: <span>August 20, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
