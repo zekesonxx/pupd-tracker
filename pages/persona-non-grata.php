4cf92e07563b361a4e7d395c3d9f47e98ea370e4
Automated <actions@users.noreply.github.com>
2025-09-09 18:47:32 +0000

    Latest data: Tue Sep  9 18:47:32 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -654,7 +654,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Monday, September 8, 2025</h3>
+  					<h3>Updated Tuesday, September 9, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -821,11 +821,6 @@
 <td width="204">Davis, Jacob K.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">4/8/29</td>
-</tr>
-<tr>
-<td width="204">De Mello, Daniel</td>
-<td width="211">DSAI</td>
-<td width="76">9/9/25</td>
 </tr>
 <tr>
 <td width="204">DeVaney, Silas</td>
@@ -1802,7 +1797,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>September 8, 2025</span></em></p>
+        <p><em>Last modified: <span>September 9, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
