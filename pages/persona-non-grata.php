ec9c75ac53bbc88cdc2e08f31180c49b00aec606
Automated <actions@users.noreply.github.com>
2025-09-12 20:32:35 +0000

    Latest data: Fri Sep 12 20:32:35 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -654,7 +654,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Thursday, September 11, 2025</h3>
+  					<h3>Updated Friday, September 12, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -931,11 +931,6 @@
 <td width="204">Hogg, Michael T</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">10/20/29</td>
-</tr>
-<tr>
-<td width="204">Holsclaw, Owen</td>
-<td width="211">Windsor Halls</td>
-<td width="76">9/14/25</td>
 </tr>
 <tr>
 <td width="204">Jackson, Anthony</td>
@@ -1792,7 +1787,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>September 11, 2025</span></em></p>
+        <p><em>Last modified: <span>September 12, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
