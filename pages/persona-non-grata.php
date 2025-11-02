4ae3d93fd7a7b8c514eb5e710c309d8b82c41207
Automated <actions@users.noreply.github.com>
2025-11-02 19:12:29 +0000

    Latest data: Sun Nov  2 19:12:29 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Friday, October 31, 2025</h3>
+  					<h3>Updated Sunday, November 2, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -706,6 +706,11 @@
 <td width="204">Berliner, Aaron J.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">11/13/25</td>
+</tr>
+<tr>
+<td width="204">Bhargava, Aadvik</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">10/31/26</td>
 </tr>
 <tr>
 <td width="204">Billingsley, Casey L.</td>
@@ -941,6 +946,11 @@
 <td width="204">Khichi, Ali A.</td>
 <td width="211">All Residence Halls and Dining Courts</td>
 <td width="76">2/14/26</td>
+</tr>
+<tr>
+<td width="204">Kichukov, Nicholas</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">10/31/26</td>
 </tr>
 <tr>
 <td width="204">Kujawa, Walter R.</td>
@@ -1762,7 +1772,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>October 31, 2025</span></em></p>
+        <p><em>Last modified: <span>November 2, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
