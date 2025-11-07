913f48c86e8cc959ab18b1fd6b9db30bb930c4a6
Automated <actions@users.noreply.github.com>
2025-11-07 14:50:01 +0000

    Latest data: Fri Nov  7 14:50:01 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Sunday, November 2, 2025</h3>
+  					<h3>Updated Friday, November 7, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1141,11 +1141,6 @@
 <td width="204">Sherman, Sheron E.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">6/17/29</td>
-</tr>
-<tr>
-<td width="204">Shidler, Garrett</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">11/7/25</td>
 </tr>
 <tr>
 <td width="204">Simpkins, AnnMarie</td>
@@ -1772,7 +1767,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>November 2, 2025</span></em></p>
+        <p><em>Last modified: <span>November 6, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
