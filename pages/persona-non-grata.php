efe0f233c4ce8214faa1ef5bb0876cb6440bc9b9
Automated <actions@users.noreply.github.com>
2025-11-07 16:19:23 +0000

    Latest data: Fri Nov  7 16:19:23 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -824,7 +824,7 @@
 </tr>
 <tr>
 <td width="204">Domi, Donald</td>
-<td width="211">Entire WL Purdue Campus</td>
+<td width="211">Wiley Residence Hall</td>
 <td width="76">10/30/26</td>
 </tr>
 <tr>
@@ -1767,7 +1767,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>November 6, 2025</span></em></p>
+        <p><em>Last modified: <span>November 7, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
