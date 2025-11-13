a7f5be5710811d0adde7f1cec07342c6ebc4c758
Automated <actions@users.noreply.github.com>
2025-11-13 15:20:57 +0000

    Latest data: Thu Nov 13 15:20:57 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -672,7 +672,34 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Thursday, November 13, 2025</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">2025PU01625</td>
+<td style="font-weight: 400;" width="148">11/6/25 7:30 PM</td>
+<td style="font-weight: 400;" width="148">11/12/25 1:54 PM</td>
+<td style="font-weight: 400;" width="201">1st St Towers</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">2025PU01626</td>
+<td style="font-weight: 400;" width="148">11/4/25 5:59 PM</td>
+<td style="font-weight: 400;" width="148">11/12/25 5:00 PM</td>
+<td style="font-weight: 400;" width="201">Bechtel Center</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -899,7 +926,7 @@ Will be posted soon.
 <td style="font-weight: 400;" width="148">11/8/25 1:52 PM</td>
 <td style="font-weight: 400;" width="148">11/8/25 2:09 PM</td>
 <td style="font-weight: 400;" width="201">Ross Ade Stadium</td>
-<td style="font-weight: 400;" width="189">Arrest Made</td>
+<td style="font-weight: 400;" width="189">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Theft</td>
@@ -931,7 +958,7 @@ Will be posted soon.
 <td style="font-weight: 400;" width="148">11/9/25 3:50 AM</td>
 <td style="font-weight: 400;" width="148">11/9/25 4:17 AM</td>
 <td style="font-weight: 400;" width="201">Honors College North</td>
-<td style="font-weight: 400;" width="189">Pending Prosecutor Review</td>
+<td style="font-weight: 400;" width="189">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Battery</td>
@@ -1487,7 +1514,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>November 12, 2025</span></em></p>
+        <p><em>Last modified: <span>November 13, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Wednesday, November 12, 2025</h3>
+  					<h3>Updated Thursday, November 13, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -701,11 +701,6 @@
 <td width="204">Beaver, Aaron J.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">2/26/30</td>
-</tr>
-<tr>
-<td width="204">Berliner, Aaron J.</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">11/13/25</td>
 </tr>
 <tr>
 <td width="204">Bhargava, Aadvik</td>
@@ -1782,7 +1777,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>November 12, 2025</span></em></p>
+        <p><em>Last modified: <span>November 13, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
