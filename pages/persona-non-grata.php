e2d5ba8ee69c493da4e32762b791db99750042fb
Automated <actions@users.noreply.github.com>
2025-12-02 18:31:23 +0000

    Latest data: Tue Dec  2 18:31:23 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Wednesday, November 26, 2025</h3>
+  					<h3>Updated Wednesday, December 2, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1256,11 +1256,6 @@
 <td width="204">Vondersaar, Coleton</td>
 <td width="211">All Residence Halls and Dining Courts</td>
 <td width="76">1/27/26</td>
-</tr>
-<tr>
-<td width="204">Wang, Rui</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">11/19/26</td>
 </tr>
 <tr>
 <td width="204">Watkins, Matthew A.</td>
@@ -1893,7 +1888,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>November 26, 2025</span></em></p>
+        <p><em>Last modified: <span>December 2, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
