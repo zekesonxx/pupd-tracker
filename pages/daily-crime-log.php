7714d1b6a3a50e2cbbcedf70480a2ce31619a3e0
Automated <actions@users.noreply.github.com>
2025-12-03 20:23:21 +0000

    Latest data: Wed Dec  3 20:23:21 UTC 2025

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -679,7 +679,50 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, December 3, 2025</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td>Criminal Mischief</td>
+<td>2025PU01745</td>
+<td>12/2/25 8:55 PM</td>
+<td>12/2/25 9:10 PM</td>
+<td>Shreve Hall</td>
+<td>Under Investigation</td>
+</tr>
+<tr>
+<td>Theft</td>
+<td>2025PU01746</td>
+<td>11/26/25 5:38 PM</td>
+<td>12/2/25 10:17 PM</td>
+<td>Aspire Apts</td>
+<td>Under Investigation</td>
+</tr>
+<tr>
+<td width="171">Reckless Driver</td>
+<td width="172">CAD# 2025-185441</td>
+<td width="148">12/1/25 9:27 PM</td>
+<td width="148">12/1/25 9:27 PM</td>
+<td width="201">400 Blk N Martin Jischke Dr</td>
+<td width="189">Nothing Further</td>
+</tr>
+<tr>
+<td width="171">Reckless Driver</td>
+<td width="172">CAD# 2025-185445</td>
+<td width="148">12/1/25 9:34 PM</td>
+<td width="148">12/1/25 9:34 PM</td>
+<td width="201">Airport Rd/ Aviation Dr</td>
+<td width="189">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1530,7 +1573,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>December 2, 2025</span></em></p>
+        <p><em>Last modified: <span>December 3, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
