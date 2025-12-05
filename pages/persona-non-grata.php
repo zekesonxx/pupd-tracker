80f3c32e05a34fcae13577e1ab6bce552f970704
Automated <actions@users.noreply.github.com>
2025-12-05 15:49:29 +0000

    Latest data: Fri Dec  5 15:49:29 UTC 2025

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Wednesday, December 3, 2025</h3>
+  					<h3>Updated Friday, December 5, 2025</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -821,6 +821,11 @@
 <td width="204">Domi, Donald</td>
 <td width="211">Wiley Residence Hall</td>
 <td width="76">10/30/26</td>
+</tr>
+<tr>
+<td width="204">Dotson, Robert J.</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">12/5/30</td>
 </tr>
 <tr>
 <td width="204">Espey, Jacob D.</td>
@@ -1883,7 +1888,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>December 3, 2025</span></em></p>
+        <p><em>Last modified: <span>December 5, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
