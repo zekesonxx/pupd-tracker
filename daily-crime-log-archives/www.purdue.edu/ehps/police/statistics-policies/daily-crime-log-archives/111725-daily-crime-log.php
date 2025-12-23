c0efd67295c3df299d1e57ffb0a7566c18a3df39
Automated <actions@users.noreply.github.com>
2025-12-23 15:49:55 +0000

    Latest data: Tue Dec 23 15:49:55 UTC 2025

--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/111725-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/111725-daily-crime-log.php
@@ -1006,7 +1006,7 @@
 <td style="font-weight: 400;" width="148">11/17/25 10:55 PM</td>
 <td style="font-weight: 400;" width="148">11/18/25 4:41 AM</td>
 <td style="font-weight: 400;" width="201">Aspire Apts</td>
-<td style="font-weight: 400;" width="189">Under Investigation</td>
+<td style="font-weight: 400;" width="189">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Drug Law Violation</td>
@@ -1200,7 +1200,7 @@
 <td style="font-weight: 400;" width="148">11/16/25 3:45 AM</td>
 <td style="font-weight: 400;" width="148">11/16/25 6:11 AM</td>
 <td style="font-weight: 400;" width="201">Dudley Hall</td>
-<td style="font-weight: 400;" width="189">Under Investigation</td>
+<td style="font-weight: 400;" width="189">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Criminal Mischief</td>
@@ -1874,7 +1874,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>December 8, 2025</span></em></p>
+        <p><em>Last modified: <span>December 23, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -686,7 +686,50 @@
   
     	<section class="content__group">
   					<h3>Tuesday, December 23, 2025</h3>
-<p>Will be posted soon.</p>
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Drug Law Violation</td>
+<td style="font-weight: 400;" width="172">2025PU01846</td>
+<td style="font-weight: 400;" width="148">12/22/25 11:35 AM</td>
+<td style="font-weight: 400;" width="148">12/22/25 12:16 PM</td>
+<td style="font-weight: 400;" width="201">400 Block Mitch Daniels Blvd</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">2025PU01847</td>
+<td style="font-weight: 400;" width="148">12/20/25 2:00 PM</td>
+<td style="font-weight: 400;" width="148">12/22/25 5:38 PM</td>
+<td style="font-weight: 400;" width="201">CoRec</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Reckless Driver</td>
+<td style="font-weight: 400;" width="172">CAD# 2025-196044</td>
+<td style="font-weight: 400;" width="148">12/20/25 7:53 PM</td>
+<td style="font-weight: 400;" width="148">12/20/25 7:53 PM</td>
+<td style="font-weight: 400;" width="201">Northwestern Ave/W Stadium Ave</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Drug Law Violation</td>
+<td style="font-weight: 400;" width="172">CAD# 2025-196304</td>
+<td style="font-weight: 400;" width="148">12/21/25 12:14 PM</td>
+<td style="font-weight: 400;" width="148">12/21/25 12:14 PM</td>
+<td style="font-weight: 400;" width="201">Meredith South Hall</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1343,7 +1386,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>December 22, 2025</span></em></p>
+        <p><em>Last modified: <span>December 23, 2025</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2025 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
