a82f7c7c96915ae17a1560c30a9a9049560e2b9b
Automated <actions@users.noreply.github.com>
2026-01-20 20:49:03 +0000

    Latest data: Tue Jan 20 20:49:03 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Monday, January 12, 2026</h3>
+  					<h3>Updated Tuesday, January 20, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -756,6 +756,11 @@
 <td width="204">Carter, Trey</td>
 <td width="211">All Residence Halls and Dining Courts&#160;</td>
 <td width="76">9/4/26</td>
+</tr>
+<tr>
+<td width="204">Chou, Yu-Chen</td>
+<td width="211">Purdue Aviation</td>
+<td width="76">1/20/27</td>
 </tr>
 <tr>
 <td width="204">Choudhury, Soumitra</td>
@@ -1888,7 +1893,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>January 12, 2026</span></em></p>
+        <p><em>Last modified: <span>January 20, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
