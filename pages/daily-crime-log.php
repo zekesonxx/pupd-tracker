944c863b7aa38fc7504d2579a261139823bfb84a
Automated <actions@users.noreply.github.com>
2026-01-28 16:36:10 +0000

    Latest data: Wed Jan 28 16:36:10 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -679,7 +679,50 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, January 28, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="172">2026PU00095</td>
+<td style="font-weight: 400;" width="148">1/23/26 1:00 PM</td>
+<td style="font-weight: 400;" width="148">1/27/26 12:02 PM</td>
+<td style="font-weight: 400;" width="201">Hagle Hall</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">2026PU00096</td>
+<td style="font-weight: 400;" width="148">1/26/26 4:30 PM</td>
+<td style="font-weight: 400;" width="148">1/27/26 2:10 PM</td>
+<td style="font-weight: 400;" width="201">CoRec</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Driving While Suspended</td>
+<td style="font-weight: 400;" width="172">2026PU00097</td>
+<td style="font-weight: 400;" width="148">1/27/26 7:47 PM</td>
+<td style="font-weight: 400;" width="148">1/27/26 8:29 PM</td>
+<td style="font-weight: 400;" width="201">Northwestern Ave/ Dodge St</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">2026PU00098</td>
+<td style="font-weight: 400;" width="148">1/27/26 9:08 AM</td>
+<td style="font-weight: 400;" width="148">1/28/26 12:16 AM</td>
+<td style="font-weight: 400;" width="201">Wiley Hall</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1386,7 +1429,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>January 27, 2026</span></em></p>
+        <p><em>Last modified: <span>January 28, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
