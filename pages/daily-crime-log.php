9f0320c3ebae51933ef6c59be0f9e96d44604e5e
Automated <actions@users.noreply.github.com>
2026-02-04 17:09:05 +0000

    Latest data: Wed Feb  4 17:09:05 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -679,7 +679,34 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, February 4, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-EMPV</td>
+<td style="font-weight: 400;" width="172">2026PU00137</td>
+<td style="font-weight: 400;" width="148">1/27/26 12:00 PM</td>
+<td style="font-weight: 400;" width="148">2/3/26 11:54 AM</td>
+<td style="font-weight: 400;" width="201">Aspire Apts</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Harassment/ Residential Entry</td>
+<td style="font-weight: 400;" width="172">2026PU00139</td>
+<td style="font-weight: 400;" width="148">2/1/26 2:01 AM</td>
+<td style="font-weight: 400;" width="148">2/3/26 3:28 PM</td>
+<td style="font-weight: 400;" width="201">Alpha Kappa Lambda</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -768,7 +795,7 @@ Will be posted soon.
 <td style="font-weight: 400;" width="148">1/27/26 10:00 AM</td>
 <td style="font-weight: 400;" width="148">1/30/26 11:15 AM</td>
 <td style="font-weight: 400;" width="201">Biochemistry Building</td>
-<td style="font-weight: 400;" width="189">Under Investigation</td>
+<td style="font-weight: 400;" width="189">Suspended</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Criminal Mischief</td>
@@ -1418,7 +1445,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>February 3, 2026</span></em></p>
+        <p><em>Last modified: <span>February 4, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
