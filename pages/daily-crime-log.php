8d985f45f102d035e5acb661e79630f083fb0522
Automated <actions@users.noreply.github.com>
2026-02-11 17:13:05 +0000

    Latest data: Wed Feb 11 17:13:05 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -679,7 +679,50 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, February 11, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="201">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;">Theft</td>
+<td style="font-weight: 400;">2026PU00176</td>
+<td style="font-weight: 400;">2/8/26 3:05 AM</td>
+<td style="font-weight: 400;">2/10/26 2:19 PM</td>
+<td style="font-weight: 400;">Honors College North</td>
+<td style="font-weight: 400;">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;">Fraud</td>
+<td style="font-weight: 400;">2026PU00177</td>
+<td style="font-weight: 400;">2/7/26 3:59 PM</td>
+<td style="font-weight: 400;">2/10/26 4:23 PM</td>
+<td style="font-weight: 400;">Aspire Apts</td>
+<td style="font-weight: 400;">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;">Theft</td>
+<td style="font-weight: 400;">2026PU00178</td>
+<td style="font-weight: 400;">2/9/26 8:44 PM</td>
+<td style="font-weight: 400;">2/10/26 4:59 PM</td>
+<td style="font-weight: 400;">3rd &amp; West Apts</td>
+<td style="font-weight: 400;">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Hit &amp; Run</td>
+<td style="font-weight: 400;" width="172">CAD# 2026-021388</td>
+<td style="font-weight: 400;" width="148">2/9/26 5:03 PM</td>
+<td style="font-weight: 400;" width="148">2/9/26 5:03 PM</td>
+<td style="font-weight: 400;" width="201">University St Parking Garage</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1506,7 +1549,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>February 10, 2026</span></em></p>
+        <p><em>Last modified: <span>February 11, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,13 +659,18 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Thursday, February 5, 2026</h3>
+  					<h3>Updated Wednesday, February 11, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
 <th scope="col" width="204">Name</th>
 <th scope="col" width="211">Location Where Valid</th>
 <th scope="col" width="76">Expiration Date</th>
+</tr>
+<tr>
+<td width="204">Adams, Michael F.</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">2/10/27</td>
 </tr>
 <tr>
 <td width="204">Arroyo, Paolo N.</td>
@@ -1903,7 +1908,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>February 5, 2026</span></em></p>
+        <p><em>Last modified: <span>February 11, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
