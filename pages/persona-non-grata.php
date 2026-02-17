8734ba7f247623c54490ad3dec6af5c35734c3bf
Automated <actions@users.noreply.github.com>
2026-02-17 20:36:40 +0000

    Latest data: Tue Feb 17 20:36:40 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Friday, February 13, 2026</h3>
+  					<h3>Updated Tuesday, February 17, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1061,11 +1061,6 @@
 <td width="204">Osborne, Caden</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">5/17/29</td>
-</tr>
-<tr>
-<td width="204">Palma, Joshua M.</td>
-<td width="211">All University Residences and Dining Courts</td>
-<td width="76">2/17/26</td>
 </tr>
 <tr>
 <td width="204">Paradis, Daniel L.</td>
@@ -1898,7 +1893,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>February 12, 2026</span></em></p>
+        <p><em>Last modified: <span>February 16, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
