fa58657e5a21d8ffa27b3e1e9f622137d3cbc293
Automated <actions@users.noreply.github.com>
2026-02-24 14:48:30 +0000

    Latest data: Tue Feb 24 14:48:30 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Monday, February 23, 2026</h3>
+  					<h3>Updated Tuesday, February 24, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1288,11 +1288,6 @@
 <td width="76">8/9/26</td>
 </tr>
 <tr>
-<td width="204">Wineinger, Reece</td>
-<td width="211">All Residence Halls and Dining Courts</td>
-<td width="76">2/24/26</td>
-</tr>
-<tr>
 <td width="204">Yang, Haiying</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">11/12/26</td>
@@ -1893,7 +1888,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>February 23, 2026</span></em></p>
+        <p><em>Last modified: <span>February 24, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
