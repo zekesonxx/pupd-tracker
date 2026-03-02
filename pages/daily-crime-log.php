0d6a43c494f8ac05c065e52a7eac28a658524b14
Automated <actions@users.noreply.github.com>
2026-03-02 20:00:52 +0000

    Latest data: Mon Mar  2 20:00:52 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -693,7 +693,106 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Monday, March 2, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="204">General Location</th>
+<th scope="col" style="text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td width="171">Theft</td>
+<td width="172">2026PU00273</td>
+<td width="148">2/23/26 8:18 PM</td>
+<td width="148">2/27/26 11:54 AM</td>
+<td width="204">Shreve Hall</td>
+<td width="189">Arrest Made</td>
+</tr>
+<tr>
+<td width="171">Theft-Bike</td>
+<td width="172">2026PU00275</td>
+<td width="148">2/27/26 2:30 PM</td>
+<td width="148">2/27/26 4:46 PM</td>
+<td width="204">Wilmeth Active Learning Center</td>
+<td width="189">Under Investigation</td>
+</tr>
+<tr>
+<td width="171">Drug Law Violation/Minor Consumption</td>
+<td width="172">2026PU00276</td>
+<td width="148">2/27/26 10:11 PM</td>
+<td width="148">2/27/26 10:42 PM</td>
+<td width="204">Wiley Hall</td>
+<td width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+<tr>
+<td width="171">Minor Consumption</td>
+<td width="172">2026PU00277</td>
+<td width="148">2/28/26 12:13 AM</td>
+<td width="148">2/28/26 12:31 AM</td>
+<td width="204">Honors College</td>
+<td width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+<tr>
+<td width="171">Trespass</td>
+<td width="172">2026PU00278</td>
+<td width="148">2/27/26 11:50 PM</td>
+<td width="148">2/28/26 1:21 AM</td>
+<td width="204">McCutcheon Hall</td>
+<td width="189">Arrest Made</td>
+</tr>
+<tr>
+<td width="171">Theft-EMPV</td>
+<td width="172">2026PU00279</td>
+<td width="148">2/27/26 1:19 PM</td>
+<td width="148">2/28/26 11:13 AM</td>
+<td width="204">Wilmeth Active Learning Center</td>
+<td width="189">Under Investigation</td>
+</tr>
+<tr>
+<td width="171">Minor Consumption</td>
+<td width="172">2026PU00280</td>
+<td width="148">3/1/26 12:51 AM</td>
+<td width="148">3/1/26 1:22 AM</td>
+<td width="204">Northwestern Parking Garage</td>
+<td width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+<tr>
+<td width="171">Minor Consumption</td>
+<td width="172">2026PU00282</td>
+<td width="148">3/1/26 1:07 AM</td>
+<td width="148">3/1/26 2:20 AM</td>
+<td width="204">Wiley Hall</td>
+<td width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+<tr>
+<td width="171">Criminal Mischief</td>
+<td width="172">2026PU00284</td>
+<td width="148">3/1/26 3:00 AM</td>
+<td width="148">3/1/26 3:52 PM</td>
+<td width="204">Tarkington Hall</td>
+<td width="189">Under Investigation</td>
+</tr>
+<tr>
+<td width="171">Habitual Traffic Violation</td>
+<td width="172">CAD# 2026-031119</td>
+<td width="148">2/26/26 9:56 PM</td>
+<td width="148">2/26/26 9:56 PM</td>
+<td width="204">S Chauncey Ave/W State St</td>
+<td width="189">Documented in 2026WL00426</td>
+</tr>
+<tr>
+<td width="171">Liquour Law Violation</td>
+<td width="172">CSA</td>
+<td width="148">2/27/26 11:10 AM</td>
+<td width="148">2/28/26 1:00 AM</td>
+<td width="204">North Hall</td>
+<td width="189">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1276,7 +1375,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>February 27, 2026</span></em></p>
+        <p><em>Last modified: <span>March 2, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
