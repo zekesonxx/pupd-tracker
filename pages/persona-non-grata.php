d0715d9c5b53357cf7dbdc61fe8b233357349c63
Automated <actions@users.noreply.github.com>
2026-04-13 19:12:57 +0000

    Latest data: Mon Apr 13 19:12:57 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -659,7 +659,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Wednesday, April 8, 2026</h3>
+  					<h3>Updated Monday, April 13, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -1091,6 +1091,11 @@
 <td width="204">P., T. (Juvenile)</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">8/9/26</td>
+</tr>
+<tr>
+<td width="204">P., I. (Juvenile)</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">4/11/31</td>
 </tr>
 <tr>
 <td width="204">Poore, Phillip W.</td>
@@ -1893,7 +1898,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>April 8, 2026</span></em></p>
+        <p><em>Last modified: <span>April 13, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
