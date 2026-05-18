3ea75fc5566d77d8c57a29c29513a83f1a59f9d3
Automated <actions@users.noreply.github.com>
2026-05-18 16:04:11 +0000

    Latest data: Mon May 18 16:04:11 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -626,11 +626,6 @@
         
                 
     
-    <li role="none"><a href="daily-crime-log.php" role="menuitem">West Lafayette Daily Crime Log</a></li>
-                                                                            
-        
-                
-    
     <li role="none"><a href="indy-daily-crime-log.php" role="menuitem">Indianapolis Daily Crime Log</a></li>
                                                                             
         
@@ -719,7 +714,66 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Monday, May 18, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="204">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Hit &amp; Run</td>
+<td style="font-weight: 400;" width="172">2026PU00680</td>
+<td style="font-weight: 400;" width="148">5/15/26 9:00 AM</td>
+<td style="font-weight: 400;" width="148">5/15/26 3:46 PM</td>
+<td style="font-weight: 400;" width="204">400 Blk Steely St</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">2026PU00682</td>
+<td style="font-weight: 400;" width="148">5/15/26 3:00 PM</td>
+<td style="font-weight: 400;" width="148">5/15/26 5:18 PM</td>
+<td style="font-weight: 400;" width="204">Alpha Chi Ro</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Drug Law Violation</td>
+<td style="font-weight: 400;" width="172">2026PU00684</td>
+<td style="font-weight: 400;" width="148">5/15/26 11:16 PM</td>
+<td style="font-weight: 400;" width="148">5/15/26 11:39 PM</td>
+<td style="font-weight: 400;" width="204">US 231/ Mitch Daniels Blvd</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Fraud</td>
+<td style="font-weight: 400;" width="172">2026PU00686</td>
+<td style="font-weight: 400;" width="148">5/15/26 12:00 PM</td>
+<td style="font-weight: 400;" width="148">5/16/26 9:22 PM</td>
+<td style="font-weight: 400;" width="204">Kappa Kappa Gamma</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-EMPV</td>
+<td style="font-weight: 400;" width="172">2026PU00688</td>
+<td style="font-weight: 400;" width="148">5/15/26 10:00 AM</td>
+<td style="font-weight: 400;" width="148">5/17/26 2:53 PM</td>
+<td style="font-weight: 400;" width="204">First St Towers</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Driving While Suspended/ Drug Law Violation</td>
+<td style="font-weight: 400;" width="172">2026PU00689</td>
+<td style="font-weight: 400;" width="148">5/17/26 11:22 PM</td>
+<td style="font-weight: 400;" width="148">5/17/26 11:46 PM</td>
+<td style="font-weight: 400;" width="204">S Grant St/ Harrison St</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1302,7 +1356,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>May 15, 2026</span></em></p>
+        <p><em>Last modified: <span>May 18, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
