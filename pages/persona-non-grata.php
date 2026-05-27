1d6a666e80847442ed1a1c86f1b91ccee3ba7d0f
Automated <actions@users.noreply.github.com>
2026-05-27 20:37:42 +0000

    Latest data: Wed May 27 20:37:42 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -739,11 +739,6 @@
 <td width="76">4/17/27</td>
 </tr>
 <tr>
-<td width="204">Bergdoll, Owen J.</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">5/12/27</td>
-</tr>
-<tr>
 <td width="204">Billingsley, Casey L.</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">7/30/26</td>
@@ -1899,7 +1894,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>May 26, 2026</span></em></p>
+        <p><em>Last modified: <span>May 27, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
