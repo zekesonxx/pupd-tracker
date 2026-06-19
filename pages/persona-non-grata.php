d268eb57705f2de9340c0a80d0e993df70af48df
Automated <actions@users.noreply.github.com>
2026-06-19 17:26:48 +0000

    Latest data: Fri Jun 19 17:26:48 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -685,7 +685,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Thursday, May 28, 2026</h3>
+  					<h3>Updated Friday, June 19, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -922,6 +922,11 @@
 <td width="204">Harper, Ethan</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">7/26/29</td>
+</tr>
+<tr>
+<td width="204">Harper, Victor C.</td>
+<td width="211">Entire WL Purdue Campus</td>
+<td width="76">6/19/27</td>
 </tr>
 <tr>
 <td width="204">Higgs, Ryan</td>
@@ -1899,7 +1904,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>May 28, 2026</span></em></p>
+        <p><em>Last modified: <span>June 19, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
