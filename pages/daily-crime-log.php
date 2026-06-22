284b8b4083b66404c7e0cde75473fa07587c17d0
Automated <actions@users.noreply.github.com>
2026-06-22 13:14:22 +0000

    Latest data: Mon Jun 22 13:14:22 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -755,7 +755,42 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Monday, June 22, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="204">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Hit &amp; Run</td>
+<td style="font-weight: 400;" width="172">2026PU00747</td>
+<td style="font-weight: 400;" width="148">6/19/26 8:00 AM</td>
+<td style="font-weight: 400;" width="148">6/19/26</td>
+<td style="font-weight: 400;" width="204">Physical Facilities Service</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Driving While Suspended</td>
+<td style="font-weight: 400;" width="172">2026PU00749</td>
+<td style="font-weight: 400;" width="148">6/20/26 4:21 PM</td>
+<td style="font-weight: 400;" width="148">6/20/26 4:55 PM</td>
+<td style="font-weight: 400;" width="204">N Russell St/N Mitch Daniels Blvd</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="172">2026PU00751</td>
+<td style="font-weight: 400;" width="148">5/1/26 11:00 PM</td>
+<td style="font-weight: 400;" width="148">6/21/26 3:47 PM</td>
+<td style="font-weight: 400;" width="204">Crosswalk Common Apts</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1338,7 +1373,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>June 19, 2026</span></em></p>
+        <p><em>Last modified: <span>June 22, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
