eec0510ff16491f1190faa3d13a05fe9fe5dad04
Automated <actions@users.noreply.github.com>
2026-07-01 20:17:43 +0000

    Latest data: Wed Jul  1 20:17:43 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -1331,7 +1331,7 @@
 <tr>
 <td width="204">Zhang, Xiao</td>
 <td width="211">Entire WL Purdue Campus</td>
-<td width="76">6/30/26</td>
+<td width="76">6/30/27</td>
 </tr>
 <tr>
 <td width="204">Zhou, Bei</td>
@@ -1919,7 +1919,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>June 30, 2026</span></em></p>
+        <p><em>Last modified: <span>July 1, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
