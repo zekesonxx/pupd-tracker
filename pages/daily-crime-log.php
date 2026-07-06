c99e9a1aeadc4fb0dfcdedf66e65979572939659
Automated <actions@users.noreply.github.com>
2026-07-06 18:10:33 +0000

    Latest data: Mon Jul  6 18:10:33 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -755,7 +755,50 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Monday, July 6, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="204">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Drug Law Violation</td>
+<td style="font-weight: 400;" width="172">2026PU00798</td>
+<td style="font-weight: 400;" width="148">7/3/26 8:34 AM</td>
+<td style="font-weight: 400;" width="148">7/3/26 8:49 AM</td>
+<td style="font-weight: 400;" width="204">US 231/Cherry Ln</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-EMPV</td>
+<td style="font-weight: 400;" width="172">2026PU00799</td>
+<td style="font-weight: 400;" width="148">6/27/26 10:34 PM</td>
+<td style="font-weight: 400;" width="148">7/4/26 12:26 PM</td>
+<td style="font-weight: 400;" width="204">Knoy Hall</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Driving While Suspended</td>
+<td style="font-weight: 400;" width="172">2026PU00800</td>
+<td style="font-weight: 400;" width="148">7/5/26 1:08 PM</td>
+<td style="font-weight: 400;" width="148">7/5/26 1:36 PM</td>
+<td style="font-weight: 400;" width="204">US 231/Cherry Ln</td>
+<td style="font-weight: 400;" width="189">Arrest Made</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Drug Law Violation</td>
+<td style="font-weight: 400;" width="172">CAD# 2026-099687</td>
+<td style="font-weight: 400;" width="148">6/27/26 9:03 PM</td>
+<td style="font-weight: 400;" width="148">6/27/26 9:03 PM</td>
+<td style="font-weight: 400;" width="204">Northwestern Ave/W Stadium Ave</td>
+<td style="font-weight: 400;" width="189">Documented in 2026WL01549</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1338,7 +1381,7 @@ Will be posted soon.
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 2, 2026</span></em></p>
+        <p><em>Last modified: <span>July 6, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
