f3fe04e749d6af0cc66d98e5cd53f154e8257943
Automated <actions@users.noreply.github.com>
2026-07-09 13:33:58 +0000

    Latest data: Thu Jul  9 13:33:58 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -698,7 +698,7 @@
   
     	<section class="content__group">
   					<h3>Thursday, July 9, 2026</h3>
-Will be posted soon.
+Nothing to report.
 		</section>
 	
           <div class="clearfix"></div>
@@ -845,7 +845,7 @@ td
 <td width="148">7/6/26 8:07 PM</td>
 <td width="148">7/6/26 8:26 PM</td>
 <td width="204">Physics Building</td>
-<td width="189">Under Investigation</td>
+<td width="189">Suspended</td>
 </tr>
 <tr>
 <td width="171">Hit &amp; Run</td>
@@ -1505,7 +1505,7 @@ td
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 8, 2026</span></em></p>
+        <p><em>Last modified: <span>July 9, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
