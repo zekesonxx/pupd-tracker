4c07d61560ed92757615a4dfe182e94fe1242dfe
Automated <actions@users.noreply.github.com>
2026-07-29 22:15:15 +0000

    Latest data: Wed Jul 29 22:15:15 UTC 2026

--- pages/persona-non-grata.php
+++ pages/persona-non-grata.php
@@ -695,7 +695,7 @@
 	
       
     	<section class="content__group">
-  					<h3>Updated Tuesday, July 28, 2026</h3>
+  					<h3>Updated Wednesday, July 29, 2026</h3>
 <table width="100%">
 <tbody>
 <tr>
@@ -757,11 +757,6 @@
 <td width="204">B., A. (Juvenile)</td>
 <td width="211">Entire WL Purdue Campus</td>
 <td width="76">12/24/26</td>
-</tr>
-<tr>
-<td width="204">Butler, Robert</td>
-<td width="211">Entire WL Purdue Campus</td>
-<td width="76">7/29/26</td>
 </tr>
 <tr>
 <td width="204">Byers, Ethan S.</td>
@@ -1939,7 +1934,7 @@
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 28, 2026</span></em></p>
+        <p><em>Last modified: <span>July 29, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
