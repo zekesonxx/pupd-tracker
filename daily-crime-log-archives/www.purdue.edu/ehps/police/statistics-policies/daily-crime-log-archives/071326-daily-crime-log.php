5a75704c9999efaa19a09d618b2ac1ffdd70290c
Automated <actions@users.noreply.github.com>
2026-07-30 15:25:20 +0000

    Latest data: Thu Jul 30 15:25:20 UTC 2026

--- daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/071326-daily-crime-log.php
+++ daily-crime-log-archives/www.purdue.edu/ehps/police/statistics-policies/daily-crime-log-archives/071326-daily-crime-log.php
@@ -637,6 +637,11 @@
                 
     
     <li role="none"><a href="070626-daily-crime-log.php" role="menuitem">West Lafayette Daily Crime Log</a></li>
+                                                                            
+        
+                
+    
+    <li role="none"><a href="072026-daily-crime-log.php" role="menuitem">West Lafayette Daily Crime Log</a></li>
                             </ul>
 </nav>
 
@@ -824,8 +829,8 @@ td
 <td style="font-weight: 400;" width="172">2026PU00832</td>
 <td style="font-weight: 400;" width="148">7/14/26 8:30 AM</td>
 <td style="font-weight: 400;" width="148">7/14/26 12:38 PM</td>
-<td style="font-weight: 400;" width="204">672 Oval Drive West Lafayette</td>
-<td style="font-weight: 400;" width="189">Under Investigation</td>
+<td style="font-weight: 400;" width="204">University Hall</td>
+<td style="font-weight: 400;" width="189">Closed</td>
 </tr>
 <tr>
 <td style="font-weight: 400;" width="171">Identity Theft</td>
@@ -851,7 +856,7 @@ td
 <td style="font-weight: 400;" width="172">CAD# 2026-108926</td>
 <td style="font-weight: 400;" width="148">7/14/26 9:34 AM</td>
 <td style="font-weight: 400;" width="148">7/14/26 9:34 AM</td>
-<td style="font-weight: 400;" width="204">W Stadium Ave/ Northwestern Ave</td>
+<td style="font-weight: 400;" width="204">W Stadium Ave/Northwestern Ave</td>
 <td style="font-weight: 400;" width="189">Nothing Further</td>
 </tr>
 </tbody>
@@ -1523,7 +1528,7 @@ td
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 20, 2026</span></em></p>
+        <p><em>Last modified: <span>July 30, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -708,7 +708,34 @@
   
     	<section class="content__group">
   					<h3>Thursday, July 30, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; width: 171px; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<td style="font-weight: 400;" width="172">Case Number</td>
+<td style="font-weight: 400;" width="148">Date/Time Occurred</td>
+<td style="font-weight: 400;" width="148">Date/Time Reported</td>
+<td style="font-weight: 400;" width="204">General Location</td>
+<td style="font-weight: 400;" width="189">Disposition&#160;</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Road Rage</td>
+<td style="font-weight: 400;" width="172">2026PU00875</td>
+<td style="font-weight: 400;" width="148">7/22/26 1:19 PM</td>
+<td style="font-weight: 400;" width="148">7/29/26 1:49 PM</td>
+<td style="font-weight: 400;" width="204">Northwestern Ave/ W Stadium Ave</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Reckless Driver</td>
+<td style="font-weight: 400;" width="172">CAD #2026-117230</td>
+<td style="font-weight: 400;" width="148">7/29/26 1:05 AM</td>
+<td style="font-weight: 400;" width="148">7/29/26 1:05 AM</td>
+<td style="font-weight: 400;" width="204">Mitch Daniels Blvd/Sheetz St</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1454,7 +1481,7 @@ td
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>July 29, 2026</span></em></p>
+        <p><em>Last modified: <span>July 30, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
