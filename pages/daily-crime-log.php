f6c6bad73335c514486f6126cf6f1f3b22bcfe4f
Automated <actions@users.noreply.github.com>
2026-08-20 15:07:43 +0000

    Latest data: Thu Aug 20 15:07:43 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -708,7 +708,50 @@
   
     	<section class="content__group">
   					<h3>Thursday, August 20, 2026</h3>
-Will be posted soon.
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="204">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Criminal Mischief</td>
+<td style="font-weight: 400;" width="172">2026PU00953</td>
+<td style="font-weight: 400;" width="148">8/19/26 8:46 AM</td>
+<td style="font-weight: 400;" width="148">8/19/26 8:46 AM</td>
+<td style="font-weight: 400;" width="204">Forney Hall</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Minor Consumption</td>
+<td style="font-weight: 400;" width="172">2026PU00955</td>
+<td style="font-weight: 400;" width="148">8/19/26 11:13 PM</td>
+<td style="font-weight: 400;" width="148">8/19/26 11:23 PM</td>
+<td style="font-weight: 400;" width="204">N Martin Jischke Dr/ 3rd St</td>
+<td style="font-weight: 400;" width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Minor Consumption</td>
+<td style="font-weight: 400;" width="172">2026PU00956</td>
+<td style="font-weight: 400;" width="148">8/19/26 11:08 PM</td>
+<td style="font-weight: 400;" width="148">8/20/26 12:38 AM</td>
+<td style="font-weight: 400;" width="204">Honors College North</td>
+<td style="font-weight: 400;" width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Minor Consumption</td>
+<td style="font-weight: 400;" width="172">2026PU00958</td>
+<td style="font-weight: 400;" width="148">8/20/26 4:30 AM</td>
+<td style="font-weight: 400;" width="148">8/20/26 5:35 AM</td>
+<td style="font-weight: 400;" width="204">Lynn Hall</td>
+<td style="font-weight: 400;" width="189">Referred for Internal Disciplinary Review</td>
+</tr>
+</tbody>
+</table>
 		</section>
 	
           <div class="clearfix"></div>
@@ -1643,7 +1686,7 @@ td
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>August 19, 2026</span></em></p>
+        <p><em>Last modified: <span>August 20, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
