ff0449b272aafefe7967e95fa81ed26cc88be7ca
Automated <actions@users.noreply.github.com>
2026-09-02 16:00:43 +0000

    Latest data: Wed Sep  2 16:00:43 UTC 2026

--- pages/daily-crime-log.php
+++ pages/daily-crime-log.php
@@ -715,7 +715,68 @@ Will be posted soon.
   
     	<section class="content__group">
   					<h3>Wednesday, September 2, 2026</h3>
-<div ccp_infra_copy_id="919d6cf1-b96b-43a7-94cf-9b1cad261257" ccp_infra_timestamp="1783537113134" ccp_infra_user_hash="2268918294" ccp_infra_version="3" data-ccp-timestamp="1783537113134">Will be posted soon.</div>
+<div ccp_infra_copy_id="919d6cf1-b96b-43a7-94cf-9b1cad261257" ccp_infra_timestamp="1783537113134" ccp_infra_user_hash="2268918294" ccp_infra_version="3" data-ccp-timestamp="1783537113134">
+<table cellpadding="5" cellspacing="3" width="100%">
+<tbody>
+<tr>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="171">Nature</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="172">Case Number</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Occurred</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="148">Date/Time Reported</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="204">General Location</th>
+<th scope="col" style="font-weight: 400; text-align: left; vertical-align: middle;" width="189">Disposition&#160;</th>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="172">2026PU01064</td>
+<td style="font-weight: 400;" width="148">8/31/26 10:31 AM</td>
+<td style="font-weight: 400;" width="148">9/1/26 11:27 AM</td>
+<td style="font-weight: 400;" width="204">Earhart Hall</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="172">2026PU01067</td>
+<td style="font-weight: 400;" width="148">8/31/26 4:30 PM</td>
+<td style="font-weight: 400;" width="148">9/1/26 4:02 PM</td>
+<td style="font-weight: 400;" width="204">Honors College</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Harassment</td>
+<td style="font-weight: 400;" width="172">2026PU01069</td>
+<td style="font-weight: 400;" width="148">9/1/26 4:28 PM</td>
+<td style="font-weight: 400;" width="148">9/1/26 4:46 PM</td>
+<td style="font-weight: 400;" width="204">Wang Hall</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft-Bike</td>
+<td style="font-weight: 400;" width="172">2026PU01074</td>
+<td style="font-weight: 400;" width="148">8/31/26 1:00 PM</td>
+<td style="font-weight: 400;" width="148">9/2/26 2:16 AM</td>
+<td style="font-weight: 400;" width="204">Honors College</td>
+<td style="font-weight: 400;" width="189">Under Investigation</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Reckless Driver</td>
+<td style="font-weight: 400;" width="172">CAD# 2026-137150</td>
+<td style="font-weight: 400;" width="148">8/31/26 10:36 PM</td>
+<td style="font-weight: 400;" width="148">8/31/26 10:36 PM</td>
+<td style="font-weight: 400;" width="204">W State St/Pierce St</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+<tr>
+<td style="font-weight: 400;" width="171">Theft</td>
+<td style="font-weight: 400;" width="172">CAD# 2026-137581</td>
+<td style="font-weight: 400;" width="148">9/1/26 1:26 PM</td>
+<td style="font-weight: 400;" width="148">9/1/26 1:26 PM</td>
+<td style="font-weight: 400;" width="204">Discovery Park Lot</td>
+<td style="font-weight: 400;" width="189">Nothing Further</td>
+</tr>
+</tbody>
+</table>
+</div>
 <style><!--
 table
 	{mso-displayed-decimal-separator:"\.";
@@ -1638,7 +1699,7 @@ td
 
 <section class="footer__contact">
     <article class="footer__contact--list">
-        <p><em>Last modified: <span>September 1, 2026</span></em></p>
+        <p><em>Last modified: <span>September 2, 2026</span></em></p>
         <p>Purdue University, 610 Purdue Mall, West Lafayette, IN, 47907, 765-494-4600</p> 
 		<p><a href="https://www.purdue.edu/purdue/disclaimer.php" target="_blank">&copy; 2026 Purdue University</a> | <a href="https://www.purdue.edu/purdue/ea_eou_statement.php" target="_blank">An equal access/equal opportunity university</a> | <a href="https://www.purdue.edu/purdue/about/integrity_statement.php" target="_blank">Integrity Statement</a> | <a href="https://www.purdue.edu/securepurdue/security-programs/copyright-policies/reporting-alleged-copyright-infringement.php" target="_blank">Copyright Complaints</a> | <a href="https://www.purdue.edu/brand/" target="_blank">Brand Toolkit</a> | <a href="mailto:police@purdue.edu" rel="noopener" target="_blank">Maintained by Purdue University Police Department</a></p>
         
